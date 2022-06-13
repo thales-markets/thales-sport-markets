db437139ffe27c2165d5ad4be7ebb078a978e9cf
dmikic <dusan.mikic15@gmail.com>
2022-06-13 14:48:35 +0200

    Added new NBA logos & fixed MLS club logo name

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,4 +1,12 @@
 import { TAGS_LIST } from '../constants/tags';
 
 export const getTeamImageSource = (team: string, leagueTag: number) =>
-    `/logos/${TAGS_LIST.find((t) => t.id == leagueTag)?.label}/${team.trim().replaceAll(' ', '-').toLowerCase()}.png`;
+    leagueTag == 9004
+        ? `/logos/${TAGS_LIST.find((t) => t.id == leagueTag)?.label}/${team
+              .trim()
+              .replaceAll(' ', '-')
+              .toLowerCase()}.svg`
+        : `/logos/${TAGS_LIST.find((t) => t.id == leagueTag)?.label}/${team
+              .trim()
+              .replaceAll(' ', '-')
+              .toLowerCase()}.png`;
