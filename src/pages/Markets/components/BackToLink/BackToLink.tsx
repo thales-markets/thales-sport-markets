28c4893aa6ce2daaef65474ef2236dc708f40d2c
makiBaraba <markovic.ofsec@gmail.com>
2022-07-05 15:16:06 +0200

    Cursor pointer to "Back to markets"

--- src/pages/Markets/components/BackToLink/BackToLink.tsx
+++ src/pages/Markets/components/BackToLink/BackToLink.tsx
@@ -23,6 +23,7 @@ const BackToLink: React.FC<BackToLinkProps> = ({ link, text }) => {
 
 const Container = styled(FlexDivCentered)`
     margin-top: 30px;
+    cursor: pointer;
 `;
 
 const Link = styled(FlexDivCentered)`
