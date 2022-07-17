7a539670bd35f151fcdd957cacc8e94f34fff99d
Leifu Chen <leifu.chen@gmail.com>
2022-07-17 14:46:27 +0300

    Added CN translation

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,15 +1,19 @@
 import enTranslation from './en.json';
+import cnTranslation from './cn.json';
 
 export const resources = {
     en: { translation: enTranslation },
+    cn: { translation: cnTranslation },
 };
 
 export enum SupportedLanguages {
     ENGLISH = 'en',
+    CHINESE = 'cn',
 }
 
 export const LanguageNameMap = {
     [SupportedLanguages.ENGLISH]: 'English',
+    [SupportedLanguages.CHINESE]: '中文',
 };
 
-export const DEFAULT_LANGUAGE = SupportedLanguages.ENGLISH;
+export const DEFAULT_LANGUAGE = SupportedLanguages.CHINESE;
