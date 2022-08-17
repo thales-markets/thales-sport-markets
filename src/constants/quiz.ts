e04cecca087c97d05bb5c8d221a3b946a098bc8b
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-08-17 15:10:09 +0200

    Add constants for quiz

--- src/constants/quiz.ts
+++ src/constants/quiz.ts
@@ -1,2 +1,7 @@
 export const NUMBER_OF_QUESTIONS = 10;
 export const QUIZ_DURATION = 2 * 60 * 1000; // 2 minutes
+export const QUIZ_API_URL = 'https://overtime-trivia.herokuapp.com';
+export const START_QUIZ_PATH = '/start';
+export const NEXT_QUESTION_PATH = '/next/question';
+export const ANSWER_QUESTION_PATH = '/answer';
+export const FINISH_QUIZ_PATH = '/finish';
--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -43,7 +43,14 @@ import styled from 'styled-components';
 import RadioButton from 'components/fields/RadioButton';
 import TimeRemaining from 'components/TimeRemaining';
 import useInterval from 'hooks/useInterval';
-import { NUMBER_OF_QUESTIONS } from 'constants/quiz';
+import {
+    ANSWER_QUESTION_PATH,
+    FINISH_QUIZ_PATH,
+    NEXT_QUESTION_PATH,
+    NUMBER_OF_QUESTIONS,
+    QUIZ_API_URL,
+    START_QUIZ_PATH,
+} from 'constants/quiz';
 
 const Rewards: React.FC = () => {
     const { t } = useTranslation();
@@ -52,7 +59,7 @@ const Rewards: React.FC = () => {
     const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
     const isQuizStarted = useSelector((state: RootState) => getIsQuizStarted(state));
     const isQuizFinished = useSelector((state: RootState) => getIsQuizFinished(state));
-    const playerUUID = useSelector((state: RootState) => getPlayerUuid(state));
+    const playerUuid = useSelector((state: RootState) => getPlayerUuid(state));
     const currentQuizItem = useSelector((state: RootState) => getCurrentQuizItem(state));
     const currentQuestionIndex = useSelector((state: RootState) => getCurrentQuestionIndex(state));
     const currentNumberOfQuestions = useSelector((state: RootState) => getCurrentNumberOfQuestions(state));
@@ -65,15 +72,15 @@ const Rewards: React.FC = () => {
 
     const handleStartQuiz = async () => {
         try {
-            const startQuizResponse = await axios.post('https://overtime-trivia.herokuapp.com/start', {
+            const startQuizResponse = await axios.post(`${QUIZ_API_URL}${START_QUIZ_PATH}`, {
                 twitter: twitterHandle,
                 wallet: walletAddress,
             });
-            const playerUUID = startQuizResponse.data.data;
-            dispatch(startQuiz(playerUUID));
+            const playerUuid = startQuizResponse.data.data;
+            dispatch(startQuiz(playerUuid));
 
             const nextQuestionResponse = await axios.get(
-                `https://overtime-trivia.herokuapp.com/next/question?playerUUID=${playerUUID}`
+                `${QUIZ_API_URL}${NEXT_QUESTION_PATH}?playerUUID=${playerUuid}`
             );
             dispatch(addQuizItem(nextQuestionResponse.data.data));
             dispatch(nextQuestion());
@@ -84,18 +91,19 @@ const Rewards: React.FC = () => {
 
     const handleNextQuestion = async () => {
         try {
-            if (currentQuestionIndex == currentNumberOfQuestions - 1) {
-                const nextQuestionResponse = await axios.get(
-                    `https://overtime-trivia.herokuapp.com/next/question?playerUUID=${playerUUID}`
-                );
-                dispatch(addQuizItem(nextQuestionResponse.data.data));
-            }
-            if (currentQuizItem.answer && currentQuizItem.answer !== '')
-                await axios.post('https://overtime-trivia.herokuapp.com/answer', {
-                    playerUUID,
+            if (currentQuizItem.answer && currentQuizItem.answer !== '') {
+                await axios.post(`${QUIZ_API_URL}${ANSWER_QUESTION_PATH}`, {
+                    playerUUID: playerUuid,
                     answer: currentQuizItem.answer,
                     questionNumber: currentQuizItem.questionNumber,
                 });
+            }
+            if (currentQuestionIndex == currentNumberOfQuestions - 1) {
+                const nextQuestionResponse = await axios.get(
+                    `${QUIZ_API_URL}${NEXT_QUESTION_PATH}?playerUUID=${playerUuid}`
+                );
+                dispatch(addQuizItem(nextQuestionResponse.data.data));
+            }
             dispatch(nextQuestion());
         } catch (e) {
             console.log(e);
@@ -113,25 +121,18 @@ const Rewards: React.FC = () => {
     const handleFinishQuiz = async () => {
         try {
             if (isQuizStarted && !isQuizFinished) {
-                const finishQuizResponse = await axios.post('https://overtime-trivia.herokuapp.com/finish', {
-                    playerUUID,
+                const finishQuizResponse = await axios.post(`${QUIZ_API_URL}${FINISH_QUIZ_PATH}`, {
+                    playerUUID: playerUuid,
                 });
-                const points = finishQuizResponse.data.data;
-
-                dispatch(finishQuiz(points));
+                const score = finishQuizResponse.data.data;
+                dispatch(finishQuiz(score));
             }
         } catch (e) {
             console.log(e);
         }
     };
 
-    const handleNewQuiz = async () => {
-        try {
-            dispatch(resetQuiz());
-        } catch (e) {
-            console.log(e);
-        }
-    };
+    const handleNewQuiz = () => dispatch(resetQuiz());
 
     const getSubmitButton = () => {
         if (!isWalletConnected && !isQuizFinished) {
