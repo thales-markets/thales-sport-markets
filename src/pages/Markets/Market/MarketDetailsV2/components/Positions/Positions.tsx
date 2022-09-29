4897626a7e3dec410b3b470c7bbcc81dc8bc190d
makiBaraba <markovic.ofsec@gmail.com>
2022-09-29 14:49:34 +0200

    Added visibility of draw option by sports

--- src/pages/Markets/Market/MarketDetailsV2/components/Positions/Positions.tsx
+++ src/pages/Markets/Market/MarketDetailsV2/components/Positions/Positions.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { AvailablePerSide, MarketData } from 'types/markets';
 import { floorNumberToDecimals, formatCurrencyWithKey } from 'utils/formatters/number';
+import { getVisibilityOfDrawOptionByTagId } from 'utils/markets';
 import { InnerContrainer, Label, TeamOptionContainer, Value, Wrapper } from './styled-components';
 
 type PositionsProps = {
@@ -21,7 +22,7 @@ const Positions: React.FC<PositionsProps> = ({ market, selectedSide, availablePe
     const disableddHomeOption = !(market?.positions[Position.HOME]?.sides[selectedSide]?.odd > 0);
     const disabledAwayOption = !(market?.positions[Position.AWAY]?.sides[selectedSide]?.odd > 0);
 
-    // const showDrawOdds = getVisibilityOfDrawOptionByTagId(market.tags);
+    const showDrawOdds = getVisibilityOfDrawOptionByTagId(market.tags);
 
     return (
         <Wrapper>
@@ -51,32 +52,38 @@ const Positions: React.FC<PositionsProps> = ({ market, selectedSide, availablePe
                     </Value>
                 </InnerContrainer>
             </TeamOptionContainer>
-            <TeamOptionContainer disabled={disabledDrawOption}>
-                <InnerContrainer>
-                    <PositionSymbol
-                        type={2}
-                        symbolColor={ODDS_COLOR.DRAW}
-                        additionalText={{
-                            firstText: 'DRAW',
-                            firstTextStyle: { fontSize: '19px', marginLeft: '15px', textTransform: 'uppercase' },
-                        }}
-                    />
-                </InnerContrainer>
-                <InnerContrainer>
-                    <Label>{t('markets.market-details.price')}</Label>
-                    <Value>
-                        {formatCurrencyWithKey(USD_SIGN, market.positions[Position.DRAW]?.sides[selectedSide]?.odd, 2)}
-                    </Value>
-                </InnerContrainer>
-                <InnerContrainer>
-                    <Label>{t('markets.market-details.liquidity')}</Label>
-                    <Value>
-                        {availablePerSide
-                            ? floorNumberToDecimals(availablePerSide.positions[Position.DRAW].available)
-                            : '-'}
-                    </Value>
-                </InnerContrainer>
-            </TeamOptionContainer>
+            {showDrawOdds && (
+                <TeamOptionContainer disabled={disabledDrawOption}>
+                    <InnerContrainer>
+                        <PositionSymbol
+                            type={2}
+                            symbolColor={ODDS_COLOR.DRAW}
+                            additionalText={{
+                                firstText: 'DRAW',
+                                firstTextStyle: { fontSize: '19px', marginLeft: '15px', textTransform: 'uppercase' },
+                            }}
+                        />
+                    </InnerContrainer>
+                    <InnerContrainer>
+                        <Label>{t('markets.market-details.price')}</Label>
+                        <Value>
+                            {formatCurrencyWithKey(
+                                USD_SIGN,
+                                market.positions[Position.DRAW]?.sides[selectedSide]?.odd,
+                                2
+                            )}
+                        </Value>
+                    </InnerContrainer>
+                    <InnerContrainer>
+                        <Label>{t('markets.market-details.liquidity')}</Label>
+                        <Value>
+                            {availablePerSide
+                                ? floorNumberToDecimals(availablePerSide.positions[Position.DRAW].available)
+                                : '-'}
+                        </Value>
+                    </InnerContrainer>
+                </TeamOptionContainer>
+            )}
             <TeamOptionContainer disabled={disabledAwayOption}>
                 <InnerContrainer>
                     <PositionSymbol
