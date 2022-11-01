bd109637c4c02df6cbd20cb093f80404dc0c3c2b
makiBaraba <markovic.ofsec@gmail.com>
2022-11-01 22:16:33 +0100

    ProfileItem Notifications

---
+++ src/layouts/DappLayout/DappHeader/components/ProfileItem/ProfileItem.tsx
@@ -0,0 +1,63 @@
+import SPAAnchor from 'components/SPAAnchor';
+import ROUTES from 'constants/routes';
+import useClaimablePositionCountQuery from 'queries/markets/useClaimablePositionCountQuery';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { getIsWalletConnected, getNetworkId, getWalletAddress } from 'redux/modules/wallet';
+import { RootState } from 'redux/rootReducer';
+import { buildHref } from 'utils/routes';
+import {
+    ProfileContainer,
+    ProfileIconContainer,
+    ProfileIcon,
+    NotificationCount,
+    ProfileLabel,
+    Count,
+} from './styled-components';
+
+const ProfileItem: React.FC = () => {
+    const { t } = useTranslation();
+    return (
+        <SPAAnchor href={buildHref(ROUTES.Profile)}>
+            <ProfileContainer>
+                <ProfileIconWidget />
+                <ProfileLabel>{t('markets.nav-menu.items.profile')}</ProfileLabel>
+            </ProfileContainer>
+        </SPAAnchor>
+    );
+};
+
+export const ProfileIconWidget: React.FC = () => {
+    const networkId = useSelector((state: RootState) => getNetworkId(state));
+    // const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
+    const isWalletConnected = useSelector((state: RootState) => getIsWalletConnected(state));
+    const walletAddress = useSelector((state: RootState) => getWalletAddress(state))
+        ? '0xf12c220b631125425f4c69823d6187FE3C8d0999'
+        : '0xf12c220b631125425f4c69823d6187FE3C8d0999';
+    const claimablePositionsCountQuery = useClaimablePositionCountQuery(walletAddress, networkId, {
+        enabled: isWalletConnected,
+    });
+
+    const claimablePositionCount =
+        claimablePositionsCountQuery.isSuccess && claimablePositionsCountQuery.data
+            ? claimablePositionsCountQuery.data
+            : null;
+
+    console.log('claimablePositionCount ', claimablePositionCount);
+
+    return (
+        <>
+            <ProfileIconContainer>
+                <ProfileIcon />
+                {claimablePositionCount && (
+                    <NotificationCount>
+                        <Count>{claimablePositionCount}</Count>
+                    </NotificationCount>
+                )}
+            </ProfileIconContainer>
+        </>
+    );
+};
+
+export default ProfileItem;
