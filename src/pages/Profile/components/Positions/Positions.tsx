04046f42bef46dfca58a4afa37728c5d5f4aef10
makiBaraba <markovic.ofsec@gmail.com>
2022-11-01 11:47:05 +0100

    Removed test wallet address

--- src/pages/Profile/components/Positions/Positions.tsx
+++ src/pages/Profile/components/Positions/Positions.tsx
@@ -18,9 +18,10 @@ const Positions: React.FC = () => {
     // const [openClaimable, setClaimableState] = useState<boolean>(false);
     // const [openOpenPositions, setOpenState] = useState<boolean>(false);
 
-    const walletAddress = useSelector((state: RootState) => getWalletAddress(state))
-        ? '0xf12c220b631125425f4c69823d6187FE3C8d0999'
-        : '0xf12c220b631125425f4c69823d6187FE3C8d0999';
+    const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
+    // const walletAddress = useSelector((state: RootState) => getWalletAddress(state))
+    //     ? '0xf12c220b631125425f4c69823d6187FE3C8d0999'
+    //     : '0xf12c220b631125425f4c69823d6187FE3C8d0999';
     const isWalletConnected = useSelector((state: RootState) => getIsWalletConnected(state));
     const networkId = useSelector((state: RootState) => getNetworkId(state));
 
