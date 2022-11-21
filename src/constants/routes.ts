f2633690c34cf8ef2b88ad1038f5c893c8fa64bb
Sasa Lotina <lotinasa@gmail.com>
2022-11-21 19:55:16 +0100

    Revert robots

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -12,7 +12,7 @@ export const ROUTES = {
     MintWorldCupNFT: '/mint-world-cup-nft',
     Vaults: '/vaults',
     Vault: '/vaults/:vaultId',
-    TwitterFlexCard: '/FlexCardsb',
+    TwitterFlexCard: '/FlexCardsc',
 };
 export default ROUTES;
 
