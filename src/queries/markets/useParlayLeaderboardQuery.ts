ad140342326e421593f93c792a138da053ae701f
Vladan Poklopic <vladanpoklopic@gmail.com>
2023-01-01 15:14:05 +0100

    Fix parlay leaderboard

--- src/queries/markets/useParlayLeaderboardQuery.ts
+++ src/queries/markets/useParlayLeaderboardQuery.ts
@@ -8,6 +8,9 @@ import { subMilliseconds } from 'date-fns';
 import { PARLAY_LEADERBOARD_MINIMUM_GAMES, PARLAY_LEADERBOARD_START_DATE_UTC } from 'constants/markets';
 import { addMonthsToUTCDate } from 'utils/formatters/date';
 
+const MAXIMUM_QUOTE = 0.033;
+const MAXIMUM_QUOTE_PERIOD_ZERO = 0.05;
+
 const sortByTotalQuote = (a: ParlayMarket, b: ParlayMarket) => {
     let firstQuote = 1;
     a.marketQuotes.map((quote) => {
@@ -38,18 +41,79 @@ export const useParlayLeaderboardQuery = (
                         1000
                 );
 
-                const parlayMarkets = await thalesData.sportMarkets.parlayMarkets({
+                let parlayMarkets = await thalesData.sportMarkets.parlayMarkets({
                     network: networkId,
                     startPeriod,
                     endPeriod,
                     won: 'true',
                 });
 
+                if (period === 1) {
+                    const startPreviousPeriod = Math.trunc(PARLAY_LEADERBOARD_START_DATE_UTC.getTime() / 1000);
+                    const endPreviousPeriod = Math.trunc(
+                        subMilliseconds(addMonthsToUTCDate(PARLAY_LEADERBOARD_START_DATE_UTC, 1), 1).getTime() / 1000
+                    );
+
+                    const parlayMarketsPreviosPeriod = await thalesData.sportMarkets.parlayMarkets({
+                        network: networkId,
+                        startPreviousPeriod,
+                        endPreviousPeriod,
+                        won: 'true',
+                    });
+
+                    const parlayMarketsPreviosPeriodModified = parlayMarketsPreviosPeriod.filter(
+                        (market: ParlayMarket) =>
+                            market.totalQuote < MAXIMUM_QUOTE_PERIOD_ZERO ||
+                            market.sportMarkets.length > PARLAY_LEADERBOARD_MINIMUM_GAMES
+                    );
+
+                    parlayMarkets = [...parlayMarkets, ...parlayMarketsPreviosPeriodModified];
+                }
+
                 const parlayMarketsModified = parlayMarkets
                     .filter(
                         (market: ParlayMarket) =>
-                            market.sportMarkets.length >= PARLAY_LEADERBOARD_MINIMUM_GAMES || period === 0
+                            (market.sportMarkets.length >= PARLAY_LEADERBOARD_MINIMUM_GAMES && period > 0) ||
+                            (period === 0 &&
+                                market.totalQuote >= MAXIMUM_QUOTE_PERIOD_ZERO &&
+                                market.sportMarkets.length <= PARLAY_LEADERBOARD_MINIMUM_GAMES)
                     )
+                    .map((parlayMarket: ParlayMarket) => {
+                        let totalQuote = parlayMarket.totalQuote;
+                        const sportMarkets = parlayMarket.sportMarkets.map((market) => {
+                            if (market.isCanceled) {
+                                let realQuote = 1;
+                                parlayMarket.marketQuotes.map((quote) => {
+                                    realQuote = realQuote * quote;
+                                });
+
+                                const marketIndex = parlayMarket.sportMarketsFromContract.findIndex(
+                                    (sportMarketFromContract) => sportMarketFromContract === market.address
+                                );
+                                if (marketIndex > -1) {
+                                    realQuote = realQuote / parlayMarket.marketQuotes[marketIndex];
+                                    const maximumQuote = period === 0 ? MAXIMUM_QUOTE_PERIOD_ZERO : MAXIMUM_QUOTE;
+                                    totalQuote = realQuote < maximumQuote ? maximumQuote : realQuote;
+                                }
+                            }
+
+                            return {
+                                ...market,
+                                homeTeam: market.isApex
+                                    ? fixApexName(market.homeTeam)
+                                    : fixLongTeamNameString(fixDuplicatedTeamName(market.homeTeam)),
+                                awayTeam: market.isApex
+                                    ? fixApexName(market.awayTeam)
+                                    : fixLongTeamNameString(fixDuplicatedTeamName(market.awayTeam)),
+                            };
+                        });
+
+                        return {
+                            ...parlayMarket,
+                            totalQuote,
+                            sportMarkets,
+                        };
+                    })
                     .sort((a: ParlayMarket, b: ParlayMarket) =>
                         a.totalQuote !== b.totalQuote
                             ? a.totalQuote - b.totalQuote
@@ -63,17 +127,6 @@ export const useParlayLeaderboardQuery = (
                         return {
                             rank: index + 1,
                             ...parlayMarket,
-                            sportMarkets: parlayMarket.sportMarkets.map((market) => {
-                                return {
-                                    ...market,
-                                    homeTeam: market.isApex
-                                        ? fixApexName(market.homeTeam)
-                                        : fixLongTeamNameString(fixDuplicatedTeamName(market.homeTeam)),
-                                    awayTeam: market.isApex
-                                        ? fixApexName(market.awayTeam)
-                                        : fixLongTeamNameString(fixDuplicatedTeamName(market.awayTeam)),
-                                };
-                            }),
                         };
                     });
                 return parlayMarketsModified;
