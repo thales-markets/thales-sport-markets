bbc7876f21c39710a681b202be9df496b9e20b25
Vladan Poklopic <vladanpoklopic@gmail.com>
2023-02-01 09:20:48 +0100

    Add round ongoing PnL

--- src/pages/Vault/TradesHistory/TradesHistory.tsx
+++ src/pages/Vault/TradesHistory/TradesHistory.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { FlexDivColumn, FlexDivRow } from 'styles/common';
+import { Colors, FlexDivColumn, FlexDivRow } from 'styles/common';
 import { useSelector } from 'react-redux';
 import { RootState } from 'redux/rootReducer';
 import { getNetworkId } from 'redux/modules/wallet';
@@ -11,18 +11,22 @@ import TradesTable from '../TradesTable';
 import useVaultTradesQuery from 'queries/vault/useVaultTradesQuery';
 import { VaultTrades, VaultTrade } from 'types/vault';
 import SelectInput from 'components/SelectInput';
+import { VaultTradeStatus } from 'constants/vault';
+import { formatPercentageWithSign } from 'utils/formatters/number';
 
 type TradesHistoryProps = {
     vaultAddress: string;
     currentRound: number;
+    currentRoundDeposit: number;
 };
 
-const TradesHistory: React.FC<TradesHistoryProps> = ({ vaultAddress, currentRound }) => {
+const TradesHistory: React.FC<TradesHistoryProps> = ({ vaultAddress, currentRound, currentRoundDeposit }) => {
     const { t } = useTranslation();
     const isAppReady = useSelector((state: RootState) => getIsAppReady(state));
     const networkId = useSelector((state: RootState) => getNetworkId(state));
     const [vaultTrades, setVaultTrades] = useState<VaultTrades>([]);
     const [round, setRound] = useState<number>(currentRound > 0 ? currentRound - 1 : 0);
+    const [pnl, setPnl] = useState<number | undefined>(undefined);
 
     const rounds: Array<{ value: number; label: string }> = [];
     for (let index = 0; index < currentRound; index++) {
@@ -50,22 +54,50 @@ const TradesHistory: React.FC<TradesHistoryProps> = ({ vaultAddress, currentRoun
         }
     }, [vaultTradesQuery.isSuccess, vaultTradesQuery.data, round]);
 
+    useEffect(() => {
+        if (round === currentRound - 1) {
+            const initialPnlAmount = 0;
+            const pnlAmount = vaultTrades.reduce((accumulator: number, trade: VaultTrade) => {
+                return (
+                    accumulator +
+                    (trade.status === VaultTradeStatus.WIN
+                        ? trade.amount - trade.paid
+                        : trade.status === VaultTradeStatus.LOSE
+                        ? -trade.paid
+                        : 0)
+                );
+            }, initialPnlAmount);
+
+            setPnl(pnlAmount / currentRoundDeposit);
+        }
+    }, [vaultTrades, currentRoundDeposit, round, currentRound]);
+
     const noResults = vaultTrades.length === 0;
 
     return (
         <Container>
             <Header>
                 <Title>{t(`vault.trades-history.title`)}</Title>
-                {currentRound !== 0 && (
-                    <SelectContainer>
-                        <SelectInput
-                            options={rounds}
-                            handleChange={(value) => setRound(Number(value))}
-                            defaultValue={round}
-                            width={230}
-                        />
-                    </SelectContainer>
-                )}
+                <RightHeader>
+                    {!!pnl && round === currentRound - 1 && (
+                        <OngoingPnlContainer>
+                            <OngoingPnlLabel>{t('vault.pnl.ongoing-pnl')}:</OngoingPnlLabel>
+                            <OngoingPnl color={pnl === 0 ? Colors.WHITE : pnl > 0 ? Colors.GREEN : Colors.RED}>
+                                {formatPercentageWithSign(pnl)}
+                            </OngoingPnl>
+                        </OngoingPnlContainer>
+                    )}
+                    {currentRound !== 0 && (
+                        <SelectContainer>
+                            <SelectInput
+                                options={rounds}
+                                handleChange={(value) => setRound(Number(value))}
+                                defaultValue={round}
+                                width={230}
+                            />
+                        </SelectContainer>
+                    )}
+                </RightHeader>
             </Header>
             <TableContainer>
                 <TradesTable
@@ -96,6 +128,13 @@ const Container = styled(FlexDivColumn)`
 
 const Header = styled(FlexDivRow)`
     margin: 10px 18px;
+    align-items: center;
+    @media (max-width: 767px) {
+        flex-direction: column;
+    }
+`;
+
+const RightHeader = styled(FlexDivRow)`
     align-items: center;
     @media (max-width: 767px) {
         flex-direction: column;
@@ -137,6 +176,23 @@ const TableContainer = styled(FlexDivColumn)`
 
 export const SelectContainer = styled.div`
     width: 230px;
+`;
+
+const OngoingPnlContainer = styled(FlexDivRow)`
+    align-items: center;
+    margin-right: 15px;
+    @media (max-width: 767px) {
+        margin-right: 0px;
+        margin-bottom: 10px;
+    }m
+`;
+
+const OngoingPnlLabel = styled.p``;
+
+const OngoingPnl = styled.p<{ color: string }>`
+    font-weight: 600;
+    margin-left: 6px;
+    color: ${(props) => props.color};
 `;
 
 export default TradesHistory;
