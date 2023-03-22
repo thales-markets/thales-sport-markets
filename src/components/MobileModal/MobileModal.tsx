1fd5f3f7fc3313c2d9d0786e1e37ca1364ec3c12
Sasa Lotina <lotinasa@gmail.com>
2023-03-22 11:15:01 +0100

    TD-1135 Fix close icon not visible on iPhone parlay

--- src/components/MobileModal/MobileModal.tsx
+++ src/components/MobileModal/MobileModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FlexDivRow } from 'styles/common';
+import { FlexDivEnd } from 'styles/common';
 import ReactModal from 'react-modal';
 
 type MobileModalProps = {
@@ -12,7 +12,7 @@ ReactModal.setAppElement('#root');
 
 const customStyles = {
     content: {
-        top: '46%',
+        top: '50%',
         left: '50%',
         right: 'auto',
         bottom: 'auto',
@@ -38,18 +38,13 @@ export const MobileModal: React.FC<MobileModalProps> = ({ onClose, children, sho
             shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
             style={customStyles}
         >
-            <Container>
-                <Header>
-                    <FlexDivRow>{<CloseIcon onClick={onClose} />}</FlexDivRow>
-                </Header>
-                {children}
-            </Container>
+            <Header>{<CloseIcon onClick={onClose} />}</Header>
+            <Container>{children}</Container>
         </ReactModal>
     );
 };
 
 const Container = styled.div`
-    z-index: 1001;
     background: ${(props) => props.theme.background.secondary};
     overflow: auto;
     border-radius: 23px;
@@ -57,10 +52,8 @@ const Container = styled.div`
     max-height: 80vh;
 `;
 
-const Header = styled(FlexDivRow)`
-    position: absolute;
-    top: -18px;
-    right: -18px;
+const Header = styled(FlexDivEnd)`
+    margin-right: -18px;
 `;
 
 const CloseIcon = styled.i`
