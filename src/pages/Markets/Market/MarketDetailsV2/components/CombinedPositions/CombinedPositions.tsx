53f1550f171981ac1ba353c99e0974d117a8b6af
makiBaraba <markovic.ofsec@gmail.com>
2023-05-10 11:24:50 +0200

    Fix error while displaying combined positions on market details

--- src/pages/Markets/Market/MarketDetailsV2/components/CombinedPositions/CombinedPositions.tsx
+++ src/pages/Markets/Market/MarketDetailsV2/components/CombinedPositions/CombinedPositions.tsx
@@ -12,10 +12,12 @@ const CombinedPositions: React.FC<CombinedPositionsProps> = ({ combinedMarkets }
     const { t } = useTranslation();
     const [isExpanded, setIsExpanded] = useState<boolean>(true);
 
-    const MAX_POSITIONS_PER_ROW = 6;
+    const MAX_POSITIONS_PER_ROW = 4;
 
-    const rowsCount = Math.round(combinedMarkets.length / MAX_POSITIONS_PER_ROW);
-
+    const rowsCount =
+        combinedMarkets.length % MAX_POSITIONS_PER_ROW == 0
+            ? combinedMarkets.length / MAX_POSITIONS_PER_ROW
+            : Math.floor(combinedMarkets.length / MAX_POSITIONS_PER_ROW) + 1;
     return (
         <Container>
             <Header>
@@ -30,7 +32,7 @@ const CombinedPositions: React.FC<CombinedPositionsProps> = ({ combinedMarkets }
                     {Array.from({ length: rowsCount }).map((_item, rowIndex) => {
                         const startIndex = rowIndex == 0 ? rowIndex : rowIndex * MAX_POSITIONS_PER_ROW;
                         const endIndex =
-                            startIndex + MAX_POSITIONS_PER_ROW > combinedMarkets.length
+                            startIndex + MAX_POSITIONS_PER_ROW >= combinedMarkets.length
                                 ? combinedMarkets.length
                                 : startIndex + MAX_POSITIONS_PER_ROW;
                         return (
