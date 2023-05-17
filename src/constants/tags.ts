1ce4ecd1a6a2f8a039372e5715e52942f6bd1598
dmikic <dusan.mikic15@gmail.com>
2023-05-17 15:43:30 +0200

    removed icon temporarily

--- src/constants/tags.ts
+++ src/constants/tags.ts
@@ -273,7 +273,7 @@ export const TAGS_LIST: Tags = [
     {
         id: 9296,
         label: 'FIFA World Cup U20',
-        logoClass: 'icon-league league--iihf',
+        logoClass: '',
         favourite: false,
         hidden: false,
         priority: 112,
