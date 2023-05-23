8a3175c07e92c0e02044180eff2f3277c9eedb96
dmikic <dusan.mikic15@gmail.com>
2023-05-23 09:47:51 +0200

    added t20 cricket league

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -23,7 +23,8 @@ export const getTeamImageSource = (team: string, leagueTag: number) =>
     leagueTag == 9057 ||
     leagueTag == 9061 ||
     leagueTag == 9045 ||
-    leagueTag == 9296
+    leagueTag == 9296 ||
+    leagueTag == 9021
         ? `/logos/${TAGS_LIST.find((t) => t.id == leagueTag)?.label}/${team
               .trim()
               .replaceAll(' ', '-')
