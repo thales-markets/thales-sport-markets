a851fac54b163cef8c3fcd71bac09ca5f5251e6b
makiBaraba <markovic.ofsec@gmail.com>
2023-06-22 10:41:00 +0200

    AMMContractStatus query

---
+++ src/queries/markets/useAMMContractsPausedQuery.ts
@@ -0,0 +1,46 @@
+import { useQuery, UseQueryOptions } from 'react-query';
+import QUERY_KEYS from 'constants/queryKeys';
+import { NetworkId } from 'types/network';
+import networkConnector from 'utils/networkConnector';
+
+export type AMMContractsPausedData = {
+    parlayAMM: boolean;
+    singleAMM: boolean;
+};
+
+const useAMMContractsPausedQuery = (networkId: NetworkId, options?: UseQueryOptions<AMMContractsPausedData>) => {
+    return useQuery<AMMContractsPausedData>(
+        QUERY_KEYS.CheckPausedAMM(networkId),
+        async () => {
+            try {
+                const { parlayMarketsAMMContract, sportsAMMContract } = networkConnector;
+
+                if (parlayMarketsAMMContract && sportsAMMContract) {
+                    const isSportsAMMPaused = await sportsAMMContract?.paused();
+                    const isParlayAMMPaused = await parlayMarketsAMMContract?.paused();
+
+                    return {
+                        parlayAMM: isParlayAMMPaused,
+                        singleAMM: isSportsAMMPaused,
+                    };
+                }
+
+                return {
+                    parlayAMM: false,
+                    singleAMM: false,
+                };
+            } catch (e) {
+                console.log(e);
+                return {
+                    parlayAMM: false,
+                    singleAMM: false,
+                };
+            }
+        },
+        {
+            ...options,
+        }
+    );
+};
+
+export default useAMMContractsPausedQuery;
