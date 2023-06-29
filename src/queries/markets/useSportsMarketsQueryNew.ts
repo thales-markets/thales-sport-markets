3dd64f0c2cbda5fc51e5bf8c75859fed1088d9b4
dmikic <dusan.mikic15@gmail.com>
2023-06-29 12:01:31 +0200

    fixed edge case for doubling names for non-rundown markets

--- src/queries/markets/useSportsMarketsQueryNew.ts
+++ src/queries/markets/useSportsMarketsQueryNew.ts
@@ -73,8 +73,9 @@ const mapMarkets = async (allMarkets: SportMarkets, mapOnlyOpenedMarkets: boolea
     allMarkets.forEach((market) => {
         if (Number(market.tags[0]) === 0) return;
         market.maturityDate = new Date(market.maturityDate);
-        market.homeTeam = fixDuplicatedTeamName(market.homeTeam);
-        market.awayTeam = fixDuplicatedTeamName(market.awayTeam);
+        const isEnetpulseSport = ENETPULSE_SPORTS.includes(Number(market.tags[0]));
+        market.homeTeam = fixDuplicatedTeamName(market.homeTeam, isEnetpulseSport);
+        market.awayTeam = fixDuplicatedTeamName(market.awayTeam, isEnetpulseSport);
         market.sport = SPORTS_MAP[market.tags[0]];
         market.isEnetpulseRacing =
             SPORTS_TAGS_MAP['Motosport'].includes(Number(market.tags[0])) &&
