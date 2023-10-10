16ac1347e240ac8aabd9c78606673f23734fea0b
makiBaraba <markovic.ofsec@gmail.com>
2023-10-10 10:14:10 +0200

    Added map for meta data

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,3 +1,5 @@
+import { Page } from 'types/ui';
+
 const ROUTES = {
     Home: '/',
     Markets: {
@@ -15,6 +17,37 @@ const ROUTES = {
     LiquidityPool: '/liquidity-pool',
     SingleLiquidityPool: '/liquidity-pool?pool-type=single',
     ParlayLiquidityPool: '/liquidity-pool?pool-type=parlay',
+};
+
+export const PAGE_NAME_TO_META_DATA_KEYS: { [page in Page]: { title: string; description: string } } = {
+    Home: {
+        title: 'seo.home.title',
+        description: 'seo.home.description',
+    },
+    Markets: {
+        title: 'seo.markets.title',
+        description: 'seo.markets.description',
+    },
+    Vaults: {
+        title: 'seo.vaults.title',
+        description: 'seo.vaults.description',
+    },
+    ParlayLeaderboard: {
+        title: 'seo.parlay-leaderboard.title',
+        description: 'seo.parlay-leaderboard.description',
+    },
+    Referral: {
+        title: 'seo.referral.title',
+        description: 'seo.referral.description',
+    },
+    Profile: {
+        title: 'seo.profile.title',
+        description: 'seo.profile.description',
+    },
+    LiquidityPool: {
+        title: 'seo.liquidity-pool.title',
+        description: 'seo.liquidity-pool.description',
+    },
 };
 
 export default ROUTES;
--- src/types/ui.ts
+++ src/types/ui.ts
@@ -3,6 +3,8 @@ import theme from 'styles/themes/dark';
 
 export type ThemeInterface = typeof theme;
 
+export type Page = 'Home' | 'Markets' | 'Vaults' | 'ParlayLeaderboard' | 'Referral' | 'Profile' | 'LiquidityPool';
+
 export type NavMenuItem = {
     i18label: string;
     iconClass: string;
