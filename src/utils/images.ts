b14cf1dd060ce01154cc0c0f26edb513c2fdd561
dmikic <dusan.mikic15@gmail.com>
2023-11-01 18:10:43 +0100

    converted all logos to webp format

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,59 +2,22 @@ import { TAGS_LIST } from '../constants/tags';
 import { fixOneSideMarketCompetitorName } from './formatters/string';
 
 export const getTeamImageSource = (team: string, leagueTag: number) =>
-    leagueTag == 9005 ||
-    leagueTag == 9010 ||
-    leagueTag == 9012 ||
-    leagueTag == 9015 ||
-    leagueTag == 9014 ||
-    leagueTag == 9007 ||
-    leagueTag == 9016 ||
-    leagueTag == 9100 ||
-    leagueTag == 9001 ||
-    leagueTag == 9017 ||
-    leagueTag == 9018 ||
-    leagueTag == 18977 ||
-    leagueTag == 18983 ||
-    leagueTag == 19138 ||
-    leagueTag == 9020 ||
-    leagueTag == 9399 ||
-    leagueTag == 18196 ||
-    leagueTag == 9057 ||
-    leagueTag == 9061 ||
-    leagueTag == 9045 ||
-    leagueTag == 9296 ||
-    leagueTag == 9021 ||
-    leagueTag == 9050 ||
-    leagueTag == 18806 ||
-    leagueTag == 18821 ||
-    leagueTag == 9288 ||
-    leagueTag == 9042 ||
-    leagueTag == 9076 ||
-    leagueTag == 19216 ||
-    leagueTag == 9073 ||
-    leagueTag == 9409 ||
-    leagueTag == 9536 ||
-    leagueTag == 19199
-        ? `/logos/${TAGS_LIST.find((t) => t.id == leagueTag)?.label}/${team
-              .trim()
-              .replaceAll(' ', '-')
-              .toLowerCase()}.png`
-        : leagueTag == 9153 || leagueTag == 9156
-        ? `/logos/Tennis/${team.trim().replaceAll(' ', '-').toLowerCase()}.png`
+    leagueTag == 9153 || leagueTag == 9156
+        ? `/logos/Tennis/${team.trim().replaceAll(' ', '-').toLowerCase()}.webp`
         : leagueTag == 9445 || leagueTag == 9497
         ? `/logos/${TAGS_LIST.find((t) => t.id == leagueTag)?.label}/${fixOneSideMarketCompetitorName(team)
               .replaceAll(' ', '-')
-              .toLowerCase()}.png`
+              .toLowerCase()}.webp`
         : leagueTag == 109021
-        ? `/logos/PGA/${team.trim().replaceAll(' ', '-').toLowerCase()}.png`
+        ? `/logos/PGA/${team.trim().replaceAll(' ', '-').toLowerCase()}.webp`
         : leagueTag == 109121
-        ? `/logos/PGA/${fixOneSideMarketCompetitorName(team).replaceAll(' ', '-').toLowerCase()}.png`
+        ? `/logos/PGA/${fixOneSideMarketCompetitorName(team).replaceAll(' ', '-').toLowerCase()}.webp`
         : leagueTag == 9268
-        ? `/logos/Brazil-Serie-A/${team.trim().replaceAll(' ', '-').toLowerCase()}.png`
+        ? `/logos/Brazil-Serie-A/${team.trim().replaceAll(' ', '-').toLowerCase()}.webp`
         : `/logos/${TAGS_LIST.find((t) => t.id == leagueTag)?.label}/${team
               .trim()
               .replaceAll(' ', '-')
-              .toLowerCase()}.svg`;
+              .toLowerCase()}.webp`;
 
 const OVERTIME_LOGO = '/logos/overtime-logo.png';
 const OVERTIME_LOGO_DARK = '/logos/overtime-logo-dark.svg';
