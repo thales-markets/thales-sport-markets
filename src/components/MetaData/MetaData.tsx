51922be06e45e264ff8f4fb0a34c6f9b6e864437
makiBaraba <markovic.ofsec@gmail.com>
2023-11-10 13:47:43 +0100

    Changes for MetaData component

--- src/components/MetaData/MetaData.tsx
+++ src/components/MetaData/MetaData.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Page } from 'types/ui';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { PAGE_NAME_TO_META_DATA_KEYS } from 'constants/routes';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
+import { getMetaRouteItem } from 'utils/routes';
+import queryString from 'query-string';
 
-type MetaDataProps = {
-    page: Page;
-};
-
-const MetaData: React.FC<MetaDataProps> = ({ page }) => {
+const MetaData: React.FC = () => {
     const { t } = useTranslation();
+    const location = useLocation();
+    const queryParams: { title?: string } = queryString.parse(location.search);
+
+    const [dynamicTitle, setDynamicTitle] = useState<string>('');
+
+    useEffect(() => {
+        if (queryParams?.title) {
+            setDynamicTitle(queryParams?.title);
+        }
+    }, [queryParams?.title]);
+
+    const metaRoute = getMetaRouteItem(location.pathname);
 
     return (
         <Helmet>
-            <title>{t(PAGE_NAME_TO_META_DATA_KEYS[page].title)}</title>
-            <meta name="description" content={t(PAGE_NAME_TO_META_DATA_KEYS[page].description)} />
+            <title>
+                {t(PAGE_NAME_TO_META_DATA_KEYS[metaRoute].title, dynamicTitle ? { dynamicTitle } : undefined)}
+            </title>
+            <meta
+                name="description"
+                content={t(
+                    PAGE_NAME_TO_META_DATA_KEYS[metaRoute].description,
+                    dynamicTitle ? { dynamicTitle } : undefined
+                )}
+            />
         </Helmet>
     );
 };
