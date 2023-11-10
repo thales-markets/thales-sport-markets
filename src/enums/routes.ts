20bed2f052f5300653b36cdfba65936693f7029c
makiBaraba <markovic.ofsec@gmail.com>
2023-11-10 13:45:58 +0100

    MetaRoutes enum

---
+++ src/enums/routes.ts
@@ -0,0 +1,10 @@
+export enum MetaRoutes {
+    Home = 'Home',
+    Markets = 'Markets',
+    Vaults = 'Vaults',
+    ParlayLeaderboard = 'ParlayLeaderboard',
+    Referral = 'Referral',
+    Profile = 'Profile',
+    LiquidityPool = 'LiquidityPool',
+    SingleMarket = 'SingleMarket',
+}
