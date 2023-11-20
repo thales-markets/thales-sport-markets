c6f86bc6a9232a7774364044ab4c89043860ba55
makiBaraba <markovic.ofsec@gmail.com>
2023-11-20 14:04:38 +0100

    WrapperNavigation

---
+++ src/pages/Profile/components/WrapperNavigation/WrapperNavigation.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { FlexDiv } from 'styles/common';
+
+type WrapperNavigationProps = {
+    tabIndex: number;
+    onChangeTab: (index: number) => void;
+};
+
+const WrapperNavigation: React.FC<WrapperNavigationProps> = ({ tabIndex, onChangeTab }) => {
+    const { t } = useTranslation();
+
+    return (
+        <Wrapper>
+            <NavItem active={tabIndex == 0} onClick={() => onChangeTab(0)}>
+                {t('profile.wrapper-nav.my-tickets')}
+            </NavItem>
+            <NavItem active={tabIndex == 1} onClick={() => onChangeTab(1)}>
+                {t('profile.wrapper-nav.my-portfolio')}
+            </NavItem>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled(FlexDiv)`
+    width: 100%;
+    border-bottom: 2px ${(props) => props.theme.borderColor.quaternary} solid;
+    flex-direction: row;
+`;
+
+const NavItem = styled(FlexDiv)<{ active?: boolean }>`
+    align-items: center;
+    color: ${(props) =>
+        props?.active ? props.theme.button.textColor.quaternary : props.theme.button.background.tertiary};
+    font-size: 18px;
+    font-weight: 700;
+    text-align: left;
+    text-transform: uppercase;
+    padding-bottom: 10px;
+    padding-right: 40px;
+    cursor: pointer;
+`;
+
+export default WrapperNavigation;
