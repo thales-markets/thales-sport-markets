e3433cf6afda682a7bc6f6478db9e809ce249717
Gorshtak <sg31191@gmail.com>
2023-12-13 16:25:54 +0100

    removed USDC from optimism aa collaterals

--- src/pages/AARelatedPages/Withdraw/components/WithdrawalConfirmationModal/WithdrawalConfirmationModal.tsx
+++ src/pages/AARelatedPages/Withdraw/components/WithdrawalConfirmationModal/WithdrawalConfirmationModal.tsx
@@ -64,9 +64,7 @@ const WithdrawalConfirmationModal: React.FC<WithdrawalConfirmationModalProps> = 
             if (multipleCollateral && signer) {
                 const collateralContractWithSigner = multipleCollateral[token]?.connect(signer);
                 await executeBiconomyTransaction(
-                    network === 10
-                        ? '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58'
-                        : (collateralContractWithSigner?.address as string),
+                    collateralContractWithSigner?.address as string,
                     collateralContractWithSigner,
                     'transfer',
                     [withdrawalAddress, parsedAmount]
--- src/utils/contracts/multipleCollateralContract.ts
+++ src/utils/contracts/multipleCollateralContract.ts
@@ -621,7 +621,7 @@ const multipleCollateral = {
     },
     USDCe: {
         addresses: {
-            [Network.OptimismMainnet]: '0x0b2c639c533813f4aa9d7837caf62653d097ff85',
+            [Network.OptimismMainnet]: '',
             [Network.OptimismGoerli]: '',
             [Network.Arbitrum]: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
             [Network.Base]: '',
