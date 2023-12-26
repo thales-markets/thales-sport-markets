32089c3f333f7dcb18d7ed00da7ad1cb8c0c6791
Gorshtak <sg31191@gmail.com>
2023-12-26 12:07:51 +0100

    adjusted rpcs to be as on dev

--- src/pages/Root/Provider/WagmiProvider/WagmiProvider.tsx
+++ src/pages/Root/Provider/WagmiProvider/WagmiProvider.tsx
@@ -58,15 +58,11 @@ const WagmiProvider: React.FC<WagmiProviderProps> = ({ children }) => {
         [
             jsonRpcProvider({
                 rpc: (chain) => ({
-                    http:
-                        chain.id === Network.Base
-                            ? // Use Ankr as primary RPC provider on Base as Chainnode isn't available
-                              `https://rpc.ankr.com/base/${process.env.REACT_APP_ANKR_PROJECT_ID}`
-                            : !CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id]?.chainnode
-                            ? chain.rpcUrls.default.http[0]
-                            : `https://${CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id].chainnode}.chainnodes.org/${
-                                  process.env.REACT_APP_CHAINNODE_PROJECT_ID
-                              }`,
+                    http: !!CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id]?.chainnode
+                        ? `https://${CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id].chainnode}.chainnodes.org/${
+                              process.env.REACT_APP_CHAINNODE_PROJECT_ID
+                          }`
+                        : chain.rpcUrls.default.http[0],
                 }),
                 stallTimeout: STALL_TIMEOUT,
                 priority: 1,
