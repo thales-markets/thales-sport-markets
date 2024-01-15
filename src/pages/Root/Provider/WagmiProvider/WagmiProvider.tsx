cf2cf2f50e3964e7e5b2bf0b7a32f3363df567ec
makiBaraba <markovic.ofsec@gmail.com>
2024-01-15 13:17:17 +0100

    Test for Rainbow kit connectors

--- src/pages/Root/Provider/WagmiProvider/WagmiProvider.tsx
+++ src/pages/Root/Provider/WagmiProvider/WagmiProvider.tsx
@@ -13,12 +13,11 @@ import {
     trustWallet,
     walletConnectWallet,
 } from '@rainbow-me/rainbowkit/wallets';
-import { base, arbitrum, optimism, optimismGoerli } from '@wagmi/chains';
+import { arbitrum, base, optimism, optimismGoerli } from '@wagmi/chains';
 import WalletDisclaimer from 'components/WalletDisclaimer';
 import { ThemeMap } from 'constants/ui';
 import { Network } from 'enums/network';
 import { merge } from 'lodash';
-import { useMemo } from 'react';
 import { getDefaultTheme } from 'redux/modules/ui';
 import { WagmiConfig, configureChains, createClient } from 'wagmi';
 import { infuraProvider } from 'wagmi/dist/providers/infura';
@@ -52,62 +51,59 @@ const CHAIN_TO_RPC_PROVIDER_NETWORK_NAME: Record<number, RpcProvider> = {
 const theme = getDefaultTheme();
 const customTheme = merge(darkTheme(), { colors: { modalBackground: ThemeMap[theme].background.primary } });
 
+const { chains, provider } = configureChains(
+    [optimism, optimismGoerli, arbitrum, base],
+    [
+        jsonRpcProvider({
+            rpc: (chain) => ({
+                http: !!CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id]?.chainnode
+                    ? `https://${CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id].chainnode}.chainnodes.org/${
+                          process.env.REACT_APP_CHAINNODE_PROJECT_ID
+                      }`
+                    : chain.rpcUrls.default.http[0],
+            }),
+            stallTimeout: STALL_TIMEOUT,
+            priority: 1,
+        }),
+        infuraProvider({
+            apiKey: process.env.REACT_APP_INFURA_PROJECT_ID || '',
+            stallTimeout: STALL_TIMEOUT,
+            priority: process.env.REACT_APP_PRIMARY_PROVIDER_ID === 'INFURA' ? 0 : 2,
+        }),
+        publicProvider({ stallTimeout: STALL_TIMEOUT, priority: 5 }),
+    ]
+);
+
+const connectors = connectorsForWallets([
+    {
+        groupName: 'Recommended',
+        wallets: [
+            metaMaskWallet({ projectId, chains }),
+            walletConnectWallet({ projectId, chains }),
+            rabbyWallet({ chains }),
+            braveWallet({ chains }),
+            ledgerWallet({ projectId, chains }),
+            trustWallet({ projectId, chains }),
+            injectedWallet({ chains }),
+            coinbaseWallet({ appName: 'Overtime', chains }),
+            rainbowWallet({ projectId, chains }),
+            imTokenWallet({ projectId, chains }),
+            particleWallet({ chains, authType: 'google' }),
+            particleWallet({ chains, authType: 'github' }),
+            particleWallet({ chains, authType: 'apple' }),
+            particleWallet({ chains, authType: 'twitter' }),
+            particleWallet({ chains, authType: 'discord' }),
+        ],
+    },
+]);
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+});
+
 const WagmiProvider: React.FC<WagmiProviderProps> = ({ children }) => {
-    const { chains, provider } = configureChains(
-        [optimism, optimismGoerli, arbitrum, base],
-        [
-            jsonRpcProvider({
-                rpc: (chain) => ({
-                    http: !!CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id]?.chainnode
-                        ? `https://${CHAIN_TO_RPC_PROVIDER_NETWORK_NAME[chain.id].chainnode}.chainnodes.org/${
-                              process.env.REACT_APP_CHAINNODE_PROJECT_ID
-                          }`
-                        : chain.rpcUrls.default.http[0],
-                }),
-                stallTimeout: STALL_TIMEOUT,
-                priority: 1,
-            }),
-            infuraProvider({
-                apiKey: process.env.REACT_APP_INFURA_PROJECT_ID || '',
-                stallTimeout: STALL_TIMEOUT,
-                priority: process.env.REACT_APP_PRIMARY_PROVIDER_ID === 'INFURA' ? 0 : 2,
-            }),
-            publicProvider({ stallTimeout: STALL_TIMEOUT, priority: 5 }),
-        ]
-    );
-
-    const connectors = useMemo(() => {
-        return connectorsForWallets([
-            {
-                groupName: 'Recommended',
-                wallets: [
-                    metaMaskWallet({ projectId, chains }),
-                    walletConnectWallet({ projectId, chains }),
-                    rabbyWallet({ chains }),
-                    braveWallet({ chains }),
-                    ledgerWallet({ projectId, chains }),
-                    trustWallet({ projectId, chains }),
-                    injectedWallet({ chains }),
-                    coinbaseWallet({ appName: 'Overtime', chains }),
-                    rainbowWallet({ projectId, chains }),
-                    imTokenWallet({ projectId, chains }),
-                    particleWallet({ chains, authType: 'google' }),
-                    particleWallet({ chains, authType: 'github' }),
-                    particleWallet({ chains, authType: 'apple' }),
-                    particleWallet({ chains, authType: 'twitter' }),
-                    particleWallet({ chains, authType: 'discord' }),
-                ],
-            },
-        ]);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [chains]);
-
-    const wagmiClient = createClient({
-        autoConnect: true,
-        connectors,
-        provider,
-    });
-
     return (
         <WagmiConfig client={wagmiClient}>
             <RainbowKitProvider
