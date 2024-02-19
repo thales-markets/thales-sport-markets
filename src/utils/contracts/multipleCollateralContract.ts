0af9f6adbaafdc3c78873be254ff16415c9f1941
Vladan Thales <vladanpoklopic@gmail.com>
2024-02-19 12:06:08 +0100

    Fix merge errors

--- src/utils/contracts/multipleCollateralContract.ts
+++ src/utils/contracts/multipleCollateralContract.ts
@@ -697,6 +697,7 @@ const multipleCollateral = {
             [Network.OptimismGoerli]: '',
             [Network.Arbitrum]: '',
             [Network.Base]: '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA',
+            [Network.OptimismSepolia]: '',
         },
         abi,
     },
