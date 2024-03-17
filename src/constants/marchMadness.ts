910f64f0933b66b02427f71f9588038e6a875c70
Sasa Lotina <lotinasa@gmail.com>
2024-03-18 00:22:59 +0100

    Revert testing date for minting

--- src/constants/marchMadness.ts
+++ src/constants/marchMadness.ts
@@ -1206,7 +1206,7 @@ export const MAX_TOTAL_POINTS = NUMBER_OF_ROUNDS * MAX_POINTS_PER_ROUND;
 export const APPROVE_MULTIPLIER = 1.1;
 export const DEFAULT_CONVERSION_BUFFER_PERCENTAGE = 0.02; // 2%
 
-export const START_MINTING_DATE = Date.UTC(2024, 2, 17); // 18th March 2024. - after Selection Sunday
+export const START_MINTING_DATE = Date.UTC(2024, 2, 18); // 18th March 2024. - after Selection Sunday
 export const END_MINTING_DATE = 1711037437; // timestamp from marchMadness contract in seconds - used only for unsupported networks 1711036800
 
 export const PRIZE_POOL_BONUS = '5,450 ARB';
