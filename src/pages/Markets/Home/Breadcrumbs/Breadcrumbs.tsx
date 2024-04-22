3ec883fa99db100355692314371ac9f3ffacfa98
vojin5rovic <vojinp13@gmail.com>
2024-04-22 16:30:05 +0200

    Add breadcrumbs functionality

--- src/pages/Markets/Home/Breadcrumbs/Breadcrumbs.tsx
+++ src/pages/Markets/Home/Breadcrumbs/Breadcrumbs.tsx
@@ -1,22 +1,25 @@
-import { useSelector } from 'react-redux';
-import { getSportFilter, getTagFilter } from 'redux/modules/market';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSportFilter, getTagFilter, setTagFilter } from 'redux/modules/market';
 import { RootState } from 'redux/rootReducer';
 import { Breadcrumb, BreadcrumbsContainer } from './styled-components';
 
 const Breadcrumbs: React.FC = () => {
+    const dispatch = useDispatch();
     const sportFilter = useSelector((state: RootState) => getSportFilter(state));
     const tagFilter = useSelector((state: RootState) => getTagFilter(state));
 
     return (
         <BreadcrumbsContainer>
-            <Breadcrumb>{sportFilter}</Breadcrumb>
+            <Breadcrumb onClick={() => dispatch(setTagFilter([]))}>{sportFilter}</Breadcrumb>
 
             {!!tagFilter.length &&
                 tagFilter.map((tag, index) => {
                     return (
                         <>
                             {index === 0 ? ' / ' : ''}
-                            <Breadcrumb key={tag.label}>{tag.label}</Breadcrumb>
+                            <Breadcrumb onClick={() => dispatch(setTagFilter([tag]))} key={tag.label}>
+                                {tag.label}
+                            </Breadcrumb>
                             {index === tagFilter.length - 1 ? '' : ', '}
                         </>
                     );
