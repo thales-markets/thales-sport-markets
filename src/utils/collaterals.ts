f3171569d41736c771cdc8d5a01c81eeb07e9f1f
Vladan Thales <vladanpoklopic@gmail.com>
2024-04-26 15:50:45 +0200

    Fix error

--- src/utils/collaterals.ts
+++ src/utils/collaterals.ts
@@ -15,7 +15,7 @@ export const getCollateralIndex = (networkId: SupportedNetwork, currencyKey: Coi
 export const getCollateralAddress = (networkId: SupportedNetwork, index: number) =>
     multipleCollateral[getCollateral(networkId, index)]?.addresses[networkId];
 
-export const getCollateralByAddress = (collateralAddress: string, networkId: SupportedNetwork) => {
+export const getCollateralByAddress = (collateralAddress: string, networkId: number) => {
     let collateral = getDefaultCollateral(networkId);
     Object.keys(multipleCollateral).forEach((collateralKey: string) => {
         Object.values(multipleCollateral[collateralKey as Coins].addresses).forEach((address: string) => {
