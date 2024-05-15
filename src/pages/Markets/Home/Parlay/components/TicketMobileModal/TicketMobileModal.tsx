780a417fb6c9a6e5fcf46f37404aa590ff5a7a5b
Vladan Thales <vladan@thalesmarket.io>
2024-05-15 11:12:21 +0200

    Fix footer

--- src/pages/Markets/Home/Parlay/components/TicketMobileModal/TicketMobileModal.tsx
+++ src/pages/Markets/Home/Parlay/components/TicketMobileModal/TicketMobileModal.tsx
@@ -16,7 +16,7 @@ const ParylayMobileModal: React.FC<ParylayMobileModalProps> = ({ onClose }) => {
                 <Title>Ticket</Title>
             </Header>
             <CloseIcon className="icon icon--close" onClick={onClose} />
-            <Scroll height="100vh">
+            <Scroll height={`calc(100vh - 100px)`}>
                 <Parlay />
             </Scroll>
         </Container>
