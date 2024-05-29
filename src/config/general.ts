6708fdb62569d875b91dfd68aa076a13fd904c82
Vladan Thales <vladan@thalesmarket.io>
2024-05-29 15:03:48 +0200

    Fix error

--- src/config/general.ts
+++ src/config/general.ts
@@ -1,3 +1,5 @@
 export const generalConfig = {
     API_URL: 'https://api.thalesmarket.io',
 };
+
+export const noCacheConfig = { headers: { 'Cache-Control': 'no-cache', Pragma: 'no-cache', Expires: '0' } };
