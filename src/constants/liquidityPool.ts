9e2c2b2bac9b42fe734e4ea4fc4e26de161471e8
Vladan Thales <vladan@thalesmarket.io>
2024-06-03 13:30:50 +0200

    Add missing file

---
+++ src/constants/liquidityPool.ts
@@ -0,0 +1,36 @@
+import { NetworkId } from 'thales-utils';
+import { LiquidityPoolCollateral } from '../enums/liquidityPool';
+import { SupportedNetwork } from '../types/network';
+
+export const LiquidityPoolMap: Record<SupportedNetwork, any> = {
+    [NetworkId.OptimismMainnet]: {
+        [LiquidityPoolCollateral.USDC]: {
+            name: 'USDC LP (default)',
+            address: '0x0fe1044Fc8C05482102Db14368fE88791E9B8698',
+        },
+        [LiquidityPoolCollateral.WETH]: {
+            name: 'WETH LP',
+            address: '0x4f2822D4e60af7f9F70E7e45BC1941fe3461231e',
+        },
+        [LiquidityPoolCollateral.THALES]: {
+            name: 'THALES LP',
+            address: '0xE59206b08cC96Da0818522C75eE3Fd4EBB7c0A47',
+        },
+    },
+    [NetworkId.Base]: undefined,
+    [NetworkId.Arbitrum]: undefined,
+    [NetworkId.OptimismSepolia]: {
+        [LiquidityPoolCollateral.USDC]: {
+            name: 'USDC LP (default)',
+            address: '0x0fe1044Fc8C05482102Db14368fE88791E9B8698',
+        },
+        [LiquidityPoolCollateral.WETH]: {
+            name: 'WETH LP',
+            address: '0x4f2822D4e60af7f9F70E7e45BC1941fe3461231e',
+        },
+        [LiquidityPoolCollateral.THALES]: {
+            name: 'THALES LP',
+            address: '0xE59206b08cC96Da0818522C75eE3Fd4EBB7c0A47',
+        },
+    },
+};
