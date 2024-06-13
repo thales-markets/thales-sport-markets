b02f2484dc729497698db10c16fa8c9afcf6046b
Vladan Thales <vladan@thalesmarket.io>
2024-06-13 09:14:50 +0200

    Fix table on market page

--- src/pages/Markets/Market/MarketDetailsV2/components/TicketTransactions/styled-components.ts
+++ src/pages/Markets/Market/MarketDetailsV2/components/TicketTransactions/styled-components.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div<{ isOnSelectedMarket?: boolean; isOpen: boolean }>`
     max-height: ${(props) => (props.isOnSelectedMarket ? '100vh' : '441px')};
-    height: ${(props) => (props.isOpen ? '100vh' : '120px')};
+    height: ${(props) => (props.isOnSelectedMarket && !props.isOpen ? '120px' : '100vh')};
     overflow: hidden;
     margin: ${(props) =>
         !props.isOnSelectedMarket ? '20px 0px' : props.isOpen ? '20px 5px 120px 5px' : '20px 5px 20px 5px'};
