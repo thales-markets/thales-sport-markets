60f0d4bec377f295c0bbce566a4b200e345ac119
Vladan Thales <vladan@thalesmarket.io>
2024-06-21 14:34:03 +0200

    Add ticket count

--- src/pages/Markets/Home/Parlay/ParlayV2.tsx
+++ src/pages/Markets/Home/Parlay/ParlayV2.tsx
@@ -173,7 +173,12 @@ const Parlay: React.FC<ParlayProps> = ({ onSuccess }) => {
         <Container isMobile={isMobile} isWalletConnected={isWalletConnected}>
             {ticketMarkets.length > 0 ? (
                 <>
-                    {!isMobile && <Title>{t('markets.parlay.ticket-slip')}</Title>}
+                    {!isMobile && (
+                        <Title>
+                            {t('markets.parlay.ticket-slip')}
+                            <Count>{ticket.length}</Count>
+                        </Title>
+                    )}
                     <ThalesBonusContainer>
                         <ThalesBonus>{t('markets.parlay.thales-bonus-info')}</ThalesBonus>
                     </ThalesBonusContainer>
@@ -249,6 +254,15 @@ const Title = styled(FlexDivCentered)`
     text-transform: uppercase;
     height: 20px;
     margin-bottom: 10px;
+`;
+
+const Count = styled(FlexDivCentered)`
+    border-radius: 8px;
+    min-width: 20px;
+    color: ${(props) => props.theme.textColor.tertiary};
+    background: ${(props) => props.theme.background.quaternary};
+    padding: 0 5px;
+    margin-left: 6px;
 `;
 
 const ThalesBonusContainer = styled(FlexDivCentered)`
