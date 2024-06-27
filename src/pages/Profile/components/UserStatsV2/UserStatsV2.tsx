2d4bfc83f7abe1ccce55c16be20063a5ac748167
makiBaraba <markovic.ofsec@gmail.com>
2024-06-27 14:22:05 +0200

    List FreeBet collaterals in UserStats component

--- src/pages/Profile/components/UserStatsV2/UserStatsV2.tsx
+++ src/pages/Profile/components/UserStatsV2/UserStatsV2.tsx
@@ -1,3 +1,4 @@
+import useFreeBetCollateralBalanceQuery from 'queries/wallet/useFreeBetCollateralBalanceQuery';
 import useUsersStatsV2Query from 'queries/wallet/useUsersStatsV2Query';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -6,7 +7,8 @@ import { getIsWalletConnected, getNetworkId, getWalletAddress } from 'redux/modu
 import { RootState } from 'redux/rootReducer';
 import styled from 'styled-components';
 import { formatCurrencyWithSign } from 'thales-utils';
-import { USD_SIGN } from '../../../../constants/currency';
+import { Coins } from 'types/tokens';
+import { COLLATERAL_ICONS_CLASS_NAMES, USD_SIGN } from '../../../../constants/currency';
 import { FlexDivColumn, FlexDivRow } from '../../../../styles/common';
 
 const UserStats: React.FC = () => {
@@ -18,28 +20,62 @@ const UserStats: React.FC = () => {
     const userStatsQuery = useUsersStatsV2Query(walletAddress.toLowerCase(), networkId, { enabled: isWalletConnected });
     const userStats = userStatsQuery.isSuccess && userStatsQuery.data ? userStatsQuery.data : undefined;
 
+    const freeBetBalancesQuery = useFreeBetCollateralBalanceQuery(walletAddress?.toLowerCase(), networkId, {
+        enabled: isWalletConnected,
+    });
+    const freeBetBalances =
+        freeBetBalancesQuery.isSuccess && freeBetBalancesQuery.data ? freeBetBalancesQuery.data : undefined;
+
+    const isFreeBetExists = freeBetBalances && !!Object.values(freeBetBalances).find((balance) => !!balance);
+
     return (
         <Wrapper>
-            <Header>
-                <ProfileIcon className="icon icon--profile2" />
-                {t('profile.stats.profile-data')}
-            </Header>
-            <Section>
-                <Label>{t('profile.stats.total-volume')}</Label>
-                <Value>{!userStats ? '-' : formatCurrencyWithSign(USD_SIGN, userStats.volume)}</Value>
-            </Section>
-            <Section>
-                <Label>{t('profile.stats.trades')}</Label>
-                <Value>{!userStats ? '-' : userStats.trades}</Value>
-            </Section>
-            <Section>
-                <Label>{t('profile.stats.highest-win')}</Label>
-                <Value>{!userStats ? '-' : formatCurrencyWithSign(USD_SIGN, userStats.highestWin)}</Value>
-            </Section>
-            <Section>
-                <Label>{t('profile.stats.lifetime-wins')}</Label>
-                <Value>{!userStats ? '-' : userStats.lifetimeWins}</Value>
-            </Section>
+            <SectionWrapper>
+                <Header>
+                    <ProfileIcon className="icon icon--profile2" />
+                    {t('profile.stats.profile-data')}
+                </Header>
+                <Section>
+                    <Label>{t('profile.stats.total-volume')}</Label>
+                    <Value>{!userStats ? '-' : formatCurrencyWithSign(USD_SIGN, userStats.volume)}</Value>
+                </Section>
+                <Section>
+                    <Label>{t('profile.stats.trades')}</Label>
+                    <Value>{!userStats ? '-' : userStats.trades}</Value>
+                </Section>
+                <Section>
+                    <Label>{t('profile.stats.highest-win')}</Label>
+                    <Value>{!userStats ? '-' : formatCurrencyWithSign(USD_SIGN, userStats.highestWin)}</Value>
+                </Section>
+                <Section>
+                    <Label>{t('profile.stats.lifetime-wins')}</Label>
+                    <Value>{!userStats ? '-' : userStats.lifetimeWins}</Value>
+                </Section>
+            </SectionWrapper>
+            {isFreeBetExists && (
+                <SectionWrapper>
+                    <SubHeader>
+                        <SubHeaderIcon className="icon icon--gift" />
+                        {t('profile.stats.free-bet')}
+                    </SubHeader>
+                    {freeBetBalances &&
+                        Object.keys(freeBetBalances).map((currencyKey) => {
+                            return freeBetBalances[currencyKey] ? (
+                                <Section>
+                                    <SubLabel>
+                                        <CurrencyIcon className={COLLATERAL_ICONS_CLASS_NAMES[currencyKey as Coins]} />
+                                        {currencyKey}
+                                    </SubLabel>
+                                    <SubValue>
+                                        {formatCurrencyWithSign(currencyKey, freeBetBalances[currencyKey])}
+                                    </SubValue>
+                                </Section>
+                            ) : (
+                                <></>
+                            );
+                        })}
+                </SectionWrapper>
+            )}
         </Wrapper>
     );
 };
@@ -70,6 +106,8 @@ const Section = styled.div`
 `;
 
 const Label = styled.span`
+    display: flex;
+    align-items: center;
     font-weight: 400;
     font-size: 12px;
     line-height: 20px;
@@ -81,6 +119,10 @@ const Label = styled.span`
     }
 `;
 
+const SubLabel = styled(Label)`
+    color: ${(props) => props.theme.textColor.octonary};
+`;
+
 const Value = styled.span`
     font-weight: 600;
     font-size: 12px;
@@ -90,12 +132,51 @@ const Value = styled.span`
     margin-left: auto;
 `;
 
+const SubValue = styled(Value)`
+    color: ${(props) => props.theme.textColor.octonary};
+`;
+
 const ProfileIcon = styled.i`
     font-size: 20px;
     margin-right: 8px;
     font-weight: 400;
     text-transform: none;
     color: ${(props) => props.theme.textColor.septenary};
+`;
+
+const SubHeaderIcon = styled.i`
+    font-size: 20px;
+    color: ${(props) => props.theme.textColor.octonary};
+    margin-right: 4px;
+    font-weight: 400;
+    text-transform: none;
+`;
+
+const SubHeader = styled(Header)`
+    color: ${(props) => props.theme.textColor.octonary};
+    &::after,
+    &:before {
+        display: inline-block;
+        content: '';
+        border-top: 2px solid ${(props) => props.theme.borderColor.senary};
+        width: 30%;
+        margin-top: 30px;
+        margin-left: 5px;
+        margin-right: 5px;
+        transform: translateY(-1rem);
+    }
+`;
+
+const CurrencyIcon = styled.i`
+    font-size: 20px !important;
+    margin: 0px 3px;
+    font-weight: 400 !important;
+    text-transform: none !important;
+    color: ${(props) => props.theme.textColor.octonary};
+`;
+
+const SectionWrapper = styled.div`
+    width: 100%;
 `;
 
 export default UserStats;
