00c5ca58ba1cbf546d9d6319cba35f12d354677f
vojin5rovic <vojinp13@gmail.com>
2024-07-01 16:10:04 +0200

    Move particle disconnect

--- src/components/WalletInfo/WalletInfo.tsx
+++ src/components/WalletInfo/WalletInfo.tsx
@@ -5,7 +5,6 @@ import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { getIsAppReady } from 'redux/modules/app';
 import {
-    getIsConnectedViaParticle,
     getIsWalletConnected,
     getNetworkId,
     getWalletAddress,
@@ -28,7 +27,7 @@ const WalletInfo: React.FC = ({}) => {
     const isAppReady = useSelector((state: RootState) => getIsAppReady(state));
     const networkId = useSelector((state: RootState) => getNetworkId(state));
     const isWalletConnected = useSelector((state: RootState) => getIsWalletConnected(state));
-    const isConnectedViaParticle = useSelector((state: RootState) => getIsConnectedViaParticle(state));
+
     const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
     const connectWalletModalVisibility = useSelector((state: RootState) => getWalletConnectModalVisibility(state));
 
@@ -52,11 +51,7 @@ const WalletInfo: React.FC = ({}) => {
                                     <WalletAddressInfo
                                         isWalletConnected={isWalletConnected}
                                         isClickable={true}
-                                        onClick={
-                                            !isConnectedViaParticle
-                                                ? () => openAccountModal()
-                                                : () => window.open(PARTICLE_WALLET, '_blank')
-                                        }
+                                        onClick={openAccountModal}
                                     >
                                         <Text className="wallet-info">
                                             {isWalletConnected
@@ -165,7 +160,5 @@ const Currency = styled(Text)`
     font-weight: bold;
     margin-left: 2px;
 `;
-
-const PARTICLE_WALLET = 'https://wallet.particle.network/';
 
 export default WalletInfo;
