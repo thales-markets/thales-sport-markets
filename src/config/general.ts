950599388ea04e78773b9764bcdca12ea3329083
Vladan Thales <vladan@thalesmarket.io>
2024-07-01 14:46:41 +0200

    New test API

--- src/config/general.ts
+++ src/config/general.ts
@@ -1,5 +1,5 @@
 export const generalConfig = {
-    API_URL: 'https://api.thalesmarket.io',
+    API_URL: 'http://overtimemarketsv2.xyz:3002',
 };
 
 export const noCacheConfig = { headers: { 'Cache-Control': 'no-cache', Pragma: 'no-cache', Expires: '0' } };
