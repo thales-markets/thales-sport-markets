236f8323ccf8925afbfbaa293b4541dab676f4a0
Vladan Thales <vladan@thalesmarket.io>
2024-07-18 10:01:32 +0200

    Add new sports for Olympics

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -145,16 +145,18 @@ export const getLeagueFlagSource = (tagId: number | any) => {
         case League.ITALY_CUP:
             return `/logos/Countries/italy.svg`;
         case League.SUMMER_OLYMPICS_BASKETBALL:
-            return `/logos/Countries/paris2024.png`;
         case League.SUMMER_OLYMPICS_BASKETBALL_WOMEN:
-            return `/logos/Countries/paris2024.png`;
         case League.SUMMER_OLYMPICS_BASKETBALL_3X3:
-            return `/logos/Countries/paris2024.png`;
         case League.SUMMER_OLYMPICS_BASKETBALL_3X3_WOMEN:
-            return `/logos/Countries/paris2024.png`;
         case League.SUMMER_OLYMPICS_SOCCER:
-            return `/logos/Countries/paris2024.png`;
         case League.SUMMER_OLYMPICS_SOCCER_WOMEN:
+        case League.SUMMER_OLYMPICS_RUGBY:
+        case League.SUMMER_OLYMPICS_RUGBY_WOMEN:
+        case League.SUMMER_OLYMPICS_VOLEYBALL:
+        case League.SUMMER_OLYMPICS_VOLEYBALL_WOMEN:
+        case League.SUMMER_OLYMPICS_HANDBALL:
+        case League.SUMMER_OLYMPICS_HANDBALL_WOMEN:
+        case League.SUMMER_OLYMPICS_WATERPOLO:
             return `/logos/Countries/paris2024.png`;
         default:
             return `/logos/Countries/world.svg`;
