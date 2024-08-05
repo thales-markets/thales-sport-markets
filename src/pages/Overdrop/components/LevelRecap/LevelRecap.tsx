f02f0b6b92d72eff88c72c0ee0255ef72fc42ade
makiBaraba <markovic.ofsec@gmail.com>
2024-08-05 13:15:27 +0200

    Heading on LevelingTree

--- src/pages/Overdrop/components/LevelRecap/LevelRecap.tsx
+++ src/pages/Overdrop/components/LevelRecap/LevelRecap.tsx
@@ -1,32 +1,50 @@
 import { OVERDROP_LEVELS } from 'constants/overdrop';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { FlexDivRowCentered } from 'styles/common';
+import { FlexDivColumn, FlexDivRowCentered } from 'styles/common';
 import LargeBadge from '../LargeBadge';
 
 const LevelRecap: React.FC = () => {
+    const { t } = useTranslation();
     return (
         <Wrapper>
-            {OVERDROP_LEVELS.map((item, index) => {
-                return (
-                    <LargeBadge
-                        key={`${index}-level`}
-                        requiredPointsForLevel={item.minimumPoints}
-                        level={item.level}
-                        reached={item.level < 10}
-                        levelName={item.levelName}
-                        voucherAmount={item.voucherAmount}
-                    />
-                );
-            })}
+            <Heading>{t('overdrop.leveling-tree.heading')}</Heading>
+            <BadgeWrapper>
+                {OVERDROP_LEVELS.map((item, index) => {
+                    return (
+                        <LargeBadge
+                            key={`${index}-level`}
+                            requiredPointsForLevel={item.minimumPoints}
+                            level={item.level}
+                            reached={item.level < 10}
+                            levelName={item.levelName}
+                            voucherAmount={item.voucherAmount}
+                        />
+                    );
+                })}
+            </BadgeWrapper>
         </Wrapper>
     );
 };
 
-const Wrapper = styled(FlexDivRowCentered)`
-    flex-wrap: wrap;
+const Wrapper = styled(FlexDivColumn)`
     flex-grow: 4;
+    justify-content: center;
+    align-items: center;
+`;
+
+const BadgeWrapper = styled(FlexDivRowCentered)`
+    flex-wrap: wrap;
     gap: 10px;
+`;
+
+const Heading = styled.h1`
+    color: ${(props) => props.theme.overdrop.textColor.primary};
+    font-size: 30px;
+    font-weight: 400;
+    text-transform: capitalize;
+    margin: 15px 0px;
 `;
 
 export default LevelRecap;
