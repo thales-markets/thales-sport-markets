6fae44b11242273dcba674181fbe57baae934212
Sasa Lotina <lotinasa@gmail.com>
2024-08-06 10:46:31 +0200

    Remove console log

--- src/components/ConnectWalletModal/ConnectWalletModal.tsx
+++ src/components/ConnectWalletModal/ConnectWalletModal.tsx
@@ -149,7 +149,7 @@ const ConnectWalletModal: React.FC<ConnectWalletModalProps> = ({ isOpen, onClose
                             <SocialButtonsWrapper>
                                 {SUPPORTED_PARTICAL_CONNECTORS.map((item, index) => {
                                     const connector = getSpecificConnectorFromConnectorsArray(connectors, item, true);
-                                    console.log(connector, connector?.ready, index);
+
                                     if (index > 4 && index < 7 && connector && connector?.ready) {
                                         return (
                                             <Button key={index} onClick={() => handleConnect(connector)}>
