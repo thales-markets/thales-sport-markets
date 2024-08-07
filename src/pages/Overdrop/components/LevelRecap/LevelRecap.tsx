43d7d52e64fa68f624f876758707921337f03dcf
makiBaraba <markovic.ofsec@gmail.com>
2024-08-07 11:01:08 +0200

    LevelRecap with real data

--- src/pages/Overdrop/components/LevelRecap/LevelRecap.tsx
+++ src/pages/Overdrop/components/LevelRecap/LevelRecap.tsx
@@ -1,12 +1,42 @@
 import { OVERDROP_LEVELS } from 'constants/overdrop';
-import React from 'react';
+import useUserDataQuery from 'queries/overdrop/useUserDataQuery';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { getIsAppReady } from 'redux/modules/app';
+import { getWalletAddress } from 'redux/modules/wallet';
+import { RootState } from 'redux/rootReducer';
 import styled from 'styled-components';
 import { FlexDivColumn, FlexDivRowCentered } from 'styles/common';
+import { OverdropUserData } from 'types/overdrop';
+import { OverdropLevel } from 'types/ui';
+import { getCurrentLevelByPoints } from 'utils/overdrop';
 import LargeBadge from '../LargeBadge';
 
 const LevelRecap: React.FC = () => {
     const { t } = useTranslation();
+
+    const isAppReady = useSelector((state: RootState) => getIsAppReady(state));
+    const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
+
+    const userDataQuery = useUserDataQuery(walletAddress, {
+        enabled: !!isAppReady,
+    });
+
+    const userData: OverdropUserData | undefined = useMemo(() => {
+        if (userDataQuery?.isSuccess && userDataQuery?.data) {
+            return userDataQuery.data;
+        }
+        return;
+    }, [userDataQuery.data, userDataQuery?.isSuccess]);
+
+    const levelItem: OverdropLevel | undefined = useMemo(() => {
+        if (userData) {
+            const levelItem = getCurrentLevelByPoints(userData.points);
+            return levelItem;
+        }
+    }, [userData]);
+
     return (
         <Wrapper>
             <Heading>{t('overdrop.leveling-tree.heading')}</Heading>
@@ -17,7 +47,7 @@ const LevelRecap: React.FC = () => {
                             key={`${index}-level`}
                             requiredPointsForLevel={item.minimumPoints}
                             level={item.level}
-                            reached={item.level < 10}
+                            reached={levelItem ? item.level < levelItem.level : false}
                             levelName={item.levelName}
                             voucherAmount={item.voucherAmount}
                         />
