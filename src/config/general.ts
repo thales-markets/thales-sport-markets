f2b5c3577ccf149d5660500f8a95bbbe143c83f1
vojin5rovic <vojinp13@gmail.com>
2024-08-09 11:35:28 +0200

    Add overdrop url

--- src/config/general.ts
+++ src/config/general.ts
@@ -1,6 +1,6 @@
 export const generalConfig = {
     API_URL: 'https://overtimemarketsv2.xyz',
-    OVERDROP_API_URL: process.env.REACT_APP_OVERDROP_API_URL,
+    OVERDROP_API_URL: 'https://overdrop.overtimemarketsv2.xyz',
 };
 
 export const noCacheConfig = { headers: { 'Cache-Control': 'no-cache', Pragma: 'no-cache', Expires: '0' } };
