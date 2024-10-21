0ecd60fd2ce4d70d7398b2c97ec91db5bba42d9e
makiBaraba <markovic.ofsec@gmail.com>
2024-10-21 15:45:21 +0200

    useLiquidityPoolReturnQuery - ReactQuery upgrade

--- src/queries/liquidityPool/useLiquidityPoolReturnQuery.ts
+++ src/queries/liquidityPool/useLiquidityPoolReturnQuery.ts
@@ -1,24 +1,24 @@
 import QUERY_KEYS from 'constants/queryKeys';
-import { Network } from 'enums/network';
 import { orderBy } from 'lodash';
 import { useQuery, UseQueryOptions } from 'react-query';
 import thalesData from 'thales-data';
 import { LiquidityPoolReturn } from 'types/liquidityPool';
+import { QueryConfig } from 'types/network';
 
 const APR_FREQUENCY = 52;
 const arrToApy = (arr: number) => (1 + arr) ** APR_FREQUENCY - 1;
 
 const useLiquidityPoolReturnQuery = (
-    networkId: Network,
     liquidityPoolAddress: string,
-    options?: UseQueryOptions<LiquidityPoolReturn | undefined>
+    queryConfig: QueryConfig,
+    options?: Omit<UseQueryOptions<any>, 'queryKey' | 'queryFn'>
 ) => {
-    return useQuery<LiquidityPoolReturn | undefined>(
-        QUERY_KEYS.LiquidityPool.Return(networkId, liquidityPoolAddress),
-        async () => {
+    return useQuery<LiquidityPoolReturn | undefined>({
+        queryKey: QUERY_KEYS.LiquidityPool.Return(queryConfig.networkId, liquidityPoolAddress),
+        queryFn: async () => {
             try {
                 const liquidityPoolPnls = await thalesData.sportMarketsV2.liquidityPoolPnls({
-                    network: networkId,
+                    network: queryConfig.networkId,
                     liquidityPool: liquidityPoolAddress,
                 });
                 const numberOfRounds = liquidityPoolPnls.length;
@@ -41,10 +41,8 @@ const useLiquidityPoolReturnQuery = (
                 return undefined;
             }
         },
-        {
-            ...options,
-        }
-    );
+        ...options,
+    });
 };
 
 export default useLiquidityPoolReturnQuery;
