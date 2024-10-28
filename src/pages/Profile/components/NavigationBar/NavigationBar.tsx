cb06128c3b3c80927fc49990ee0773dac3499476
makiBaraba <markovic.ofsec@gmail.com>
2024-10-28 12:10:22 +0100

    Navigation bar - wagmi data

--- src/pages/Profile/components/NavigationBar/NavigationBar.tsx
+++ src/pages/Profile/components/NavigationBar/NavigationBar.tsx
@@ -3,7 +3,10 @@ import useClaimablePositionCountV2Query from 'queries/markets/useClaimablePositi
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { getIsWalletConnected, getNetworkId, getWalletAddress } from 'redux/modules/wallet';
+import { getIsBiconomy } from 'redux/modules/wallet';
+import { RootState } from 'redux/rootReducer';
+import biconomyConnector from 'utils/biconomyWallet';
+import { useAccount, useChainId, useClient } from 'wagmi';
 import { Count, Icon, Item, ItemWrapper, NotificationCount, Wrapper } from './styled-components';
 
 const navItems = [
@@ -31,13 +34,21 @@ type NavigationBarProps = {
 
 const NavigationBar: React.FC<NavigationBarProps> = ({ selectedTab, setSelectedTab }) => {
     const { t } = useTranslation();
-    const networkId = useSelector(getNetworkId);
-    const walletAddress = useSelector(getWalletAddress) || '';
-    const isWalletConnected = useSelector(getIsWalletConnected);
 
-    const claimablePositionsCountQuery = useClaimablePositionCountV2Query(walletAddress, networkId, {
-        enabled: isWalletConnected,
-    });
+    const isBiconomy = useSelector((state: RootState) => getIsBiconomy(state));
+
+    const networkId = useChainId();
+    const client = useClient();
+    const { address, isConnected } = useAccount();
+    const walletAddress = (isBiconomy ? biconomyConnector.address : address) || '';
+
+    const claimablePositionsCountQuery = useClaimablePositionCountV2Query(
+        walletAddress,
+        { networkId, client },
+        {
+            enabled: isConnected,
+        }
+    );
     const claimablePositionCount =
         claimablePositionsCountQuery.isSuccess && claimablePositionsCountQuery.data
             ? claimablePositionsCountQuery.data
