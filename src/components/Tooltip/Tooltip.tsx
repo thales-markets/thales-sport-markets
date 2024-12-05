e54cdeb9d68200d599a269323937c2a419068f5a
Sasa Lotina <lotinasa@gmail.com>
2024-12-05 17:08:14 +0100

    Fix validation tooltip for numeric input

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,7 +1,8 @@
 import ReactTooltip from 'rc-tooltip';
 import React, { CSSProperties } from 'react';
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import 'styles/tooltip.css';
+import { ThemeInterface } from 'types/ui';
 
 type TooltipProps = {
     overlay: any;
@@ -12,7 +13,8 @@ type TooltipProps = {
     top?: number;
     overlayClassName?: string;
     iconColor?: string;
-    children?: React.ReactNode;
+    children?: React.ReactElement;
+    isValidation?: boolean;
 };
 
 const Tooltip: React.FC<TooltipProps> = ({
@@ -25,16 +27,30 @@ const Tooltip: React.FC<TooltipProps> = ({
     overlayClassName,
     iconColor,
     children,
+    isValidation,
 }) => {
-    return (
+    const theme: ThemeInterface = useTheme();
+
+    return isValidation ? (
+        <ReactTooltip
+            visible
+            overlay={overlay}
+            placement="top"
+            overlayClassName={overlayClassName}
+            overlayInnerStyle={{ ...overlayInnerStyle, ...getValidationStyle(theme) }}
+            arrowContent={<ValidationArrow />}
+        >
+            {children}
+        </ReactTooltip>
+    ) : (
         <ReactTooltip
             overlay={overlay}
             placement="top"
-            overlayClassName={overlayClassName || ''}
+            overlayClassName={overlayClassName}
             overlayInnerStyle={overlayInnerStyle}
         >
             {children ? (
-                (children as any)
+                children
             ) : (
                 <InfoIcon
                     color={iconColor}
@@ -61,6 +77,42 @@ const InfoIcon = styled.i<{ iconFontSize?: number; marginLeft?: number; top?: nu
         font-family: OvertimeIconsV2 !important;
         content: '\\011B';
     }
+`;
+
+const getValidationStyle = (theme: ThemeInterface) => ({
+    minWidth: '100%',
+    maxWidth: '300px',
+    padding: '4px 8px',
+    backgroundColor: theme.error.background.primary,
+    color: theme.error.textColor.primary,
+    border: `1.5px solid ${theme.error.borderColor.primary}`,
+    borderRadius: '2px',
+    fontSize: '10px',
+    fontWeight: 600,
+    lineHeight: '14px',
+    'text-transform': 'uppercase',
+});
+
+const ValidationArrow = styled.div`
+    &:before {
+        content: '';
+        display: block;
+        width: 100%;
+        height: 100%;
+        margin: auto;
+        transform-origin: 100% 0;
+        transform: rotate(45deg);
+        border: 1.5px solid ${(props) => props.theme.error.borderColor.primary};
+        background-color: ${(props) => props.theme.error.background.primary};
+        box-sizing: border-box;
+    }
+    position: absolute;
+    overflow: hidden;
+    width: 13px;
+    height: 10px;
+    bottom: -4px;
+    top: auto;
+    right: -7px;
 `;
 
 export default Tooltip;
