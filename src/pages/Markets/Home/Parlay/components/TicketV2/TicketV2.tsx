bc5c75e8979038a34caae5706c4bdd528caac36d
Gorshtak <sg31191@gmail.com>
2024-12-06 12:43:38 +0100

    Fixed live trading

--- src/pages/Markets/Home/Parlay/components/TicketV2/TicketV2.tsx
+++ src/pages/Markets/Home/Parlay/components/TicketV2/TicketV2.tsx
@@ -1438,7 +1438,7 @@ const Ticket: React.FC<TicketProps> = ({
                                 }
                             }
 
-                            const isFulfilled = await sportsAMMV2ContractWithSigner.write.requestIdToFulfillAllowed([
+                            const isFulfilled = await sportsAMMV2ContractWithSigner.read.requestIdToFulfillAllowed([
                                 requestId,
                             ]);
                             if (!isFulfilled) {
