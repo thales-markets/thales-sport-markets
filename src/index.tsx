dcbb7f649abd50ba1288d5b0f37ce776feac8fa2
Sasa Lotina <lotinasa@gmail.com>
2024-12-16 19:19:40 +0100

    Remove double imports from index

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,9 @@ import Root from 'pages/Root';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import store from 'redux/store';
-import 'styles/currencies.css';
-import 'styles/fonts.css';
-import 'styles/homepage-icons.css';
-import 'styles/icons.css';
+
 import 'styles/main.css';
 import 'styles/overrides.css';
-import 'styles/thales-sidebar-icons.css';
-import 'styles/tooltip.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
