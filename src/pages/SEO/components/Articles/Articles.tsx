194b8c6e980a4facf5654d930e11f5d2aabb3cff
makiBaraba <markovic.ofsec@gmail.com>
2024-12-24 14:03:12 +0100

    Pagination on SEO Articles

--- src/pages/SEO/components/Articles/Articles.tsx
+++ src/pages/SEO/components/Articles/Articles.tsx
@@ -1,5 +1,6 @@
 import Loader from 'components/Loader';
-import { SEO_ARTICLES_PER_PAGE } from 'constants/ui';
+import SelectInput from 'components/SelectInput';
+import { ScreenSizeBreakpoint } from 'enums/ui';
 import DappFooter from 'layouts/DappLayout/DappFooter';
 import { useSEOArticlesQuery } from 'queries/seo/useSEOArticlesQuery';
 import React, { useEffect, useMemo, useState } from 'react';
@@ -9,6 +10,13 @@ import styled from 'styled-components';
 import { FlexDiv } from 'styles/common';
 import useQueryParam from 'utils/useQueryParams';
 import ArticleCard from '../ArticleCard';
+
+const PAGINATION_SIZE = [
+    { value: 12, label: '12' },
+    { value: 24, label: '24' },
+    { value: 36, label: '36' },
+    { value: 48, label: '48' },
+];
 
 const Articles: React.FC = () => {
     const { t } = useTranslation();
@@ -20,6 +28,8 @@ const Articles: React.FC = () => {
 
     const queryParams = useMemo(() => new URLSearchParams(location.search), [location.search]);
     const initialPage = parseInt(queryParams.get('page') || '1', 10);
+
+    const [seoArticlesPerPage, setSeoArticlesPerPage] = useState<number>(PAGINATION_SIZE[0].value);
 
     const [currentPage, setCurrentPage] = useState(initialPage);
 
@@ -43,12 +53,12 @@ const Articles: React.FC = () => {
     }, [seoArticlesQuery.data, seoArticlesQuery.isSuccess]);
 
     const paginatedArticles = useMemo(() => {
-        const startIndex = (currentPage - 1) * SEO_ARTICLES_PER_PAGE;
-        const endIndex = startIndex + SEO_ARTICLES_PER_PAGE;
+        const startIndex = (currentPage - 1) * seoArticlesPerPage;
+        const endIndex = startIndex + seoArticlesPerPage;
         return seoArticles.slice(startIndex, endIndex);
-    }, [seoArticles, currentPage]);
+    }, [currentPage, seoArticlesPerPage, seoArticles]);
 
-    const totalPages = Math.ceil(seoArticles.length / SEO_ARTICLES_PER_PAGE);
+    const totalPages = Math.ceil(seoArticles.length / seoArticlesPerPage);
 
     const handleNextPage = () => {
         if (currentPage < totalPages) {
@@ -95,17 +105,39 @@ const Articles: React.FC = () => {
                             <EmptyContainer>{t('promotions.no-promotions')}</EmptyContainer>
                         )}
                     </CardsWrapper>
-                    <PaginationControls>
-                        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-                            Previous
-                        </button>
-                        <span>
-                            Page {currentPage} of {totalPages}
-                        </span>
-                        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-                            Next
-                        </button>
-                    </PaginationControls>
+                    <PaginationWrapper>
+                        <SectionWrapper>
+                            <PaginationLabel>{t('common.pagination.rows-per-page')}</PaginationLabel>
+                            <div>
+                                <SelectInput
+                                    options={PAGINATION_SIZE}
+                                    value={{ value: seoArticlesPerPage, label: seoArticlesPerPage.toString() }}
+                                    handleChange={(value: any) => {
+                                        setCurrentPage(1);
+                                        setSeoArticlesPerPage(Number(value));
+                                    }}
+                                    isPaginationStyle
+                                />
+                            </div>
+                        </SectionWrapper>
+
+                        <SectionWrapper className="flex items-center gap-1">
+                            <PaginationLabel>
+                                {`${currentPage} ${t('common.pagination.of')} ${totalPages}`}
+                            </PaginationLabel>
+                        </SectionWrapper>
+
+                        <ActionSection>
+                            <ArrowWrapper onClick={() => handlePreviousPage()} disabled={currentPage === 1}>
+                                <ArrowLeft className={'icon icon--arrow-down'} />
+                            </ArrowWrapper>
+                        </ActionSection>
+                        <ActionSection>
+                            <ArrowWrapper onClick={() => handleNextPage()} disabled={currentPage === totalPages}>
+                                <ArrowRight className={'icon icon--arrow-down'} />
+                            </ArrowWrapper>
+                        </ActionSection>
+                    </PaginationWrapper>
                 </>
             )}
             <DappFooter />
@@ -157,30 +189,65 @@ const EmptyContainer = styled(FlexDiv)`
     width: 100%;
 `;
 
-const PaginationControls = styled.div`
+const PaginationWrapper = styled.div`
+    width: 100%;
     display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 5px 0;
+    @media (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
+        justify-content: flex-start;
+    }
+`;
+
+const SectionWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 4px;
+    margin: 0 14px;
+    @media (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
+        margin: 0 8px;
+    }
+`;
+
+const PaginationLabel = styled.p`
+    color: ${(props) => props.theme.textColor.primary};
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 10%;
+    letter-spacing: 0.13px;
+    @media (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
+        font-size: 12px;
+    }
+`;
+
+const ActionSection = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 4px;
+`;
+
+const ArrowWrapper = styled.span<{ disabled: boolean }>`
+    display: flex;
+    width: 40px;
+    height: 24px;
     justify-content: center;
     align-items: center;
-    margin: 30px 0px;
+    padding: 4px;
+    color: ${(props) => props.theme.textColor.primary};
+    opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+    cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
+`;
 
-    button {
-        margin: 0 10px;
-        padding: 10px 20px;
-        background-color: ${(props) => props.theme.button.background.quaternary};
-        color: ${(props) => props.theme.button.textColor.primary};
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-
-        &:disabled {
-            background-color: #cccccc;
-            cursor: not-allowed;
-        }
-    }
-
-    span {
-        margin: 0 10px;
-    }
+const ArrowLeft = styled.i`
+    font-size: 12px;
+    rotate: 90deg;
+`;
+const ArrowRight = styled.i`
+    font-size: 12px;
+    rotate: -90deg;
 `;
 
 export default Articles;
