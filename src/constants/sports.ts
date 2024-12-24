8cc4e1ee531933e4f3b6efbea4ee4fdb0ca61ba0
Sasa Lotina <lotinasa@gmail.com>
2024-12-24 13:14:57 +0100

    Change Brazil serie a label as duplicated with Italian label

--- src/constants/sports.ts
+++ src/constants/sports.ts
@@ -493,7 +493,7 @@ export const LeagueMap: Record<League, LeagueInfo> = {
     [League.BRAZIL_1]: {
         sport: Sport.SOCCER,
         id: League.BRAZIL_1,
-        label: 'Serie A',
+        label: 'Brazil Serie A',
         scoringType: ScoringType.GOALS,
         matchResolveType: MatchResolveType.REGULAR,
         periodType: PeriodType.HALF,
