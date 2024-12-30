641360b0b2b6941d3feea68233d16b3d3b991bb8
Sasa Lotina <lotinasa@gmail.com>
2024-12-30 10:44:49 +0100

    Add deployment error handler

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -5,10 +5,6 @@ import { isMobile } from 'utils/device';
 const DISCORD_MESSAGE_MAX_LENGTH = 2000;
 
 export const logError = (error: Error, info: ErrorInfo) => {
-    if (import.meta.env.DEV) {
-        return;
-    }
-
     let content = `IsMobile: ${isMobile()}\nError:\n${error.stack || error.message}`;
     const flags = 4; // SUPPRESS_EMBEDS
     fetch(LINKS.Discord.OvertimeErrors, {
