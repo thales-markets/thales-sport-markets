f4efbbeec935f34f107c4a6eaf3cd20ece7cb823
Gorshtak <sg31191@gmail.com>
2025-01-10 18:33:56 +0100

    added account tab in profile

--- src/enums/ui.ts
+++ src/enums/ui.ts
@@ -6,7 +6,7 @@ export enum ProfileTab {
     OPEN_CLAIMABLE = 'open-claimable',
     TRANSACTION_HISTORY = 'transaction-history',
     LP = 'lp',
-    MY_PORTFOLIO = 'my-portfolio',
+    ACCOUNT = 'account',
 }
 
 export enum PnlTab {
