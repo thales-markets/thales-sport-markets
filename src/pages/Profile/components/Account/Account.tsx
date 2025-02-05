c558c5b7b535970c1f522006db817b47c21078dc
Gorshtak <sg31191@gmail.com>
2025-02-05 11:41:14 +0100

    added asset table

--- src/pages/Profile/components/Account/Account.tsx
+++ src/pages/Profile/components/Account/Account.tsx
@@ -1,24 +1,24 @@
+import ConvertIcon from 'assets/images/svgs/convert.svg?react';
+import DepositIcon from 'assets/images/svgs/deposit.svg?react';
+import WithdrawIcon from 'assets/images/svgs/withdraw.svg?react';
 import Button from 'components/Button';
 import FundModal from 'components/FundOvertimeAccountModal';
 import SwapModal from 'components/SwapModal/SwapModal';
+import Toggle from 'components/Toggle';
 import { COLLATERAL_ICONS, OVER_SIGH, USD_SIGN } from 'constants/currency';
 import { LOCAL_STORAGE_KEYS } from 'constants/storage';
-import useLocalStorage from 'hooks/useLocalStorage';
 import { useUserTicketsQuery } from 'queries/markets/useUserTicketsQuery';
 import useExchangeRatesQuery from 'queries/rates/useExchangeRatesQuery';
 import useFreeBetCollateralBalanceQuery from 'queries/wallet/useFreeBetCollateralBalanceQuery';
 import useMultipleCollateralBalanceQuery from 'queries/wallet/useMultipleCollateralBalanceQuery';
-import queryString from 'query-string';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import { getIsBiconomy, setIsBiconomy } from 'redux/modules/wallet';
 import styled from 'styled-components';
 import {
     Colors,
     FlexDivCentered,
-    FlexDivColumn,
     FlexDivColumnCentered,
     FlexDivColumnStart,
     FlexDivEnd,
@@ -30,7 +30,6 @@ import { Rates } from 'types/collateral';
 import { RootState } from 'types/redux';
 import biconomyConnector from 'utils/biconomyWallet';
 import { getCollaterals, mapMultiCollateralBalances } from 'utils/collaterals';
-import { claimFreeBet } from 'utils/freeBet';
 import { useAccount, useChainId, useClient } from 'wagmi';
 import WithdrawModal from '../WithdrawModal';
 
@@ -41,13 +40,6 @@ const Account: React.FC = () => {
     const dispatch = useDispatch();
     const isBiconomy = useSelector((state: RootState) => getIsBiconomy(state));
 
-    const queryParams: { freeBet?: string } = queryString.parse(location.search);
-    const [freeBet, setFreeBet] = useLocalStorage<string | undefined>(
-        LOCAL_STORAGE_KEYS.FREE_BET_ID,
-        queryParams.freeBet
-    );
-    const history = useHistory();
-
     const networkId = useChainId();
     const client = useClient();
     const { address, isConnected } = useAccount();
@@ -56,6 +48,8 @@ const Account: React.FC = () => {
     const [showFundModal, setShowFundModal] = useState<boolean>(false);
     const [showWithdrawModal, setShowWithdrawModal] = useState<boolean>(false);
     const [showSwapModal, setShowSwapModal] = useState<boolean>(false);
+
+    const [showZeroBalance, setShowZeroBalance] = useState<boolean>(false);
 
     const multipleCollateralBalances = useMultipleCollateralBalanceQuery(
         walletAddress,
@@ -101,6 +95,36 @@ const Account: React.FC = () => {
         }
     }, [exchangeRates, multipleCollateralBalances.data, networkId, balanceList]);
 
+    const usersAssets = useMemo(() => {
+        try {
+            if (exchangeRates && multipleCollateralBalances.data) {
+                const result: Array<{
+                    asset: string;
+                    balance: number;
+                    value: number;
+                }> = [];
+                getCollaterals(networkId).forEach((token) => {
+                    result.push({
+                        asset: token,
+                        balance: multipleCollateralBalances.data[token],
+                        value:
+                            multipleCollateralBalances.data[token] * (exchangeRates[token] ? exchangeRates[token] : 1),
+                    });
+                });
+                return result
+                    .sort((a, b) => {
+                        return b.value - a.value;
+                    })
+                    .filter((tokenData) => {
+                        if (tokenData.value === 0 && !showZeroBalance) return false;
+                        return true;
+                    });
+            }
+        } catch (e) {
+            return [];
+        }
+    }, [exchangeRates, multipleCollateralBalances.data, networkId, showZeroBalance]);
+
     const userTicketsQuery = useUserTicketsQuery(
         walletAddress,
         { networkId, client },
@@ -118,7 +142,8 @@ const Account: React.FC = () => {
                 if (ticket.isOpen) {
                     console.log(ticket);
                     tickets++;
-                    potentialWin += ticket.payout * exchangeRates[ticket.collateral];
+                    potentialWin +=
+                        ticket.payout * (exchangeRates[ticket.collateral] ? exchangeRates[ticket.collateral] : 1);
                 }
             });
 
@@ -149,14 +174,6 @@ const Account: React.FC = () => {
         };
     }, [multipleCollateralBalances.data, exchangeRates]);
 
-    const onClaimFreeBet = useCallback(() => claimFreeBet(walletAddress, freeBet, networkId, setFreeBet, history), [
-        walletAddress,
-        freeBet,
-        setFreeBet,
-        history,
-        networkId,
-    ]);
-
     return (
         <div>
             <Header>
@@ -164,21 +181,6 @@ const Account: React.FC = () => {
                     <Label>Portfolio Balance</Label>
                     <Value>{totalBalanceValue}</Value>
                 </FlexDivColumnStart>
-                <FlexDivColumn>
-                    {!!freeBet && (
-                        <Button
-                            onClick={onClaimFreeBet}
-                            borderColor="none"
-                            height="42px"
-                            width="160px"
-                            lineHeight="16px"
-                            padding="0"
-                            backgroundColor={Colors.BLUE}
-                        >
-                            {t('profile.account-summary.claim-free-bet')}
-                        </Button>
-                    )}
-                </FlexDivColumn>
                 <FlexDivEnd gap={20}>
                     <FlexDivColumnStart gap={4}>
                         <GrayLabel>Active Tickets</GrayLabel>
@@ -245,6 +247,42 @@ const Account: React.FC = () => {
                 </FlexDivSpaceBetween>
             </FlexDivSpaceBetween>
 
+            <GridContainer>
+                <TableHeader>Assets</TableHeader>
+                <TableHeader2>Amount</TableHeader2>
+                <TableHeader2>Value(USD)</TableHeader2>
+                <ZeroBalanceWrapper>
+                    <TableHeader2>Show zero balance</TableHeader2>
+                    <Toggle active={showZeroBalance} handleClick={() => setShowZeroBalance(!showZeroBalance)} />
+                </ZeroBalanceWrapper>
+
+                {usersAssets?.map((assetData, index) => {
+                    const Icon = COLLATERAL_ICONS[assetData.asset];
+                    const StyledIcon = styled(Icon)`
+                        height: 24px;
+                        width: 30px;
+                    `;
+                    return (
+                        <AssetContainer key={index}>
+                            <AssetWrapper>
+                                {<StyledIcon />} {assetData.asset}
+                            </AssetWrapper>
+                            <Label2>{formatCurrencyWithKey('', assetData.balance)}</Label2>
+                            <Label2>{formatCurrencyWithKey(USD_SIGN, assetData.value, 2)}</Label2>
+                            <Deposit>
+                                Deposit <DepositIcon />
+                            </Deposit>
+                            <Convert>
+                                Convert <ConvertIcon />
+                            </Convert>
+                            <Withdraw>
+                                Withdraw <WithdrawIcon />
+                            </Withdraw>
+                        </AssetContainer>
+                    );
+                })}
+            </GridContainer>
+
             <SkipText
                 onClick={() => {
                     if (isBiconomy) {
@@ -273,7 +311,13 @@ const ButtonContainer = styled(FlexDivCentered)`
     gap: 18px;
 `;
 
-const SkipText = styled.p`
+const AlignedParagraph = styled.p`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const SkipText = styled(AlignedParagraph)`
     color: ${(props) => props.theme.textColor.quaternary};
     text-align: center;
     font-size: 14px;
@@ -282,7 +326,7 @@ const SkipText = styled.p`
     cursor: pointer;
 `;
 
-const Label = styled.p`
+const Label = styled(AlignedParagraph)`
     color: ${(props) => props.theme.textColor.primary};
     font-size: 14px;
     font-weight: 500;
@@ -293,7 +337,7 @@ const GrayLabel = styled(Label)`
     color: ${(props) => props.theme.button.textColor.senary};
 `;
 
-const Value = styled.p`
+const Value = styled(AlignedParagraph)`
     color: ${(props) => props.theme.textColor.quaternary};
     font-size: 24px;
     font-weight: 700;
@@ -302,22 +346,25 @@ const Value = styled.p`
 
 const YellowValue = styled(Value)`
     color: ${(props) => props.theme.overdrop.textColor.primary};
-    display: flex;
-    align-items: center;
 `;
 
 const ParlayIcon = styled.i.attrs({ className: 'icon icon--parlay' })`
     font-size: 16px;
 `;
 
-const Label2 = styled.p`
+const Label2 = styled(AlignedParagraph)`
     color: ${(props) => props.theme.textColor.primary};
     font-size: 16px;
     font-weight: 500;
     white-space: pre;
 `;
 
-const Value2 = styled.p`
+const AssetWrapper = styled(Label2)`
+    justify-content: flex-start;
+    gap: 8px;
+`;
+
+const Value2 = styled(AlignedParagraph)`
     color: ${(props) => props.theme.textColor.quaternary};
     font-size: 16px;
     font-weight: 700;
@@ -326,6 +373,61 @@ const Value2 = styled.p`
 
 const YellowValue2 = styled(Value2)`
     color: ${(props) => props.theme.overdrop.textColor.primary};
+`;
+
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 14px;
+    margin-top: 20px;
+    padding-top: 20px;
+    grid-template-rows: auto;
+    border-top: 2px solid ${(props) => props.theme.background.senary};
+`;
+
+const TableHeader = styled(AlignedParagraph)`
+    justify-content: flex-start;
+    color: ${(props) => props.theme.textColor.primary};
+    font-size: 20px;
+    font-weight: 700;
+    white-space: pre;
+`;
+
+const TableHeader2 = styled(AlignedParagraph)`
+    color: ${(props) => props.theme.button.textColor.senary};
+    font-size: 14px;
+    font-weight: 700;
+    white-space: pre;
+`;
+
+const ZeroBalanceWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    grid-column-start: 5;
+    grid-column-end: 7;
+`;
+
+const AssetContainer = styled.div`
+    display: contents;
+    grid-column: 1;
+    grid-column-end: 7;
+    border-top: 1px solid ${(props) => props.theme.background.senary};
+`;
+
+const Deposit = styled(Value)`
+    cursor: pointer;
+    font-size: 14px;
+`;
+
+const Convert = styled(YellowValue)`
+    color: white;
+    cursor: pointer;
+    font-size: 14px;
+`;
+
+const Withdraw = styled(YellowValue)`
+    cursor: pointer;
+    font-size: 14px;
 `;
 
 export default Account;
