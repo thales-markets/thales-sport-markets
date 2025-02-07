01cf1740cb359903832b109d962c6a1127334d66
Sasa Lotina <lotinasa@gmail.com>
2025-02-07 17:09:47 +0100

    Change behavior of suggested amounts

--- src/pages/Markets/Home/Parlay/components/SuggestedAmount/SuggestedAmount.tsx
+++ src/pages/Markets/Home/Parlay/components/SuggestedAmount/SuggestedAmount.tsx
@@ -1,6 +1,6 @@
 import { USD_SIGN } from 'constants/currency';
 import { THALES_CONTRACT_RATE_KEY } from 'constants/markets';
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { FlexDiv } from 'styles/common';
 import { formatCurrencyWithKey } from 'thales-utils';
@@ -8,7 +8,7 @@ import { Rates } from 'types/collateral';
 import { convertFromStableToCollateral, getCollateral, isThalesCurrency } from 'utils/collaterals';
 import { useChainId } from 'wagmi';
 
-const AMOUNTS = [3, 10, 20, 50, 100];
+const AMOUNTS = [3, 10, 50, 100, 500];
 
 type SuggestedAmountProps = {
     collateralIndex: number;
@@ -27,17 +27,50 @@ const SuggestedAmount: React.FC<SuggestedAmountProps> = ({
 }) => {
     const networkId = useChainId();
 
+    const [amountIndexClickedTimesMap, setAmountIndexClickedTimesMap] = useState(
+        new Map(AMOUNTS.map((_, i) => [i, 0]))
+    );
+    const [isAmountClicked, setIsAmountClicked] = useState(false);
+
     const collateral = useMemo(() => getCollateral(networkId, collateralIndex), [networkId, collateralIndex]);
     const isThales = isThalesCurrency(collateral);
 
     const convertFromStable = useCallback(
         (value: number) => {
             const rate = exchangeRates?.[isThales ? THALES_CONTRACT_RATE_KEY : collateral] || 0;
-
             return convertFromStableToCollateral(collateral, value, rate, networkId);
         },
         [collateral, networkId, exchangeRates, isThales]
     );
+
+    // Reset clicked times for all amounts
+    const prevInsertedAmount = useRef<number>(Number(insertedAmount));
+    useEffect(() => {
+        const isInsertedAmountChanged = prevInsertedAmount.current !== Number(insertedAmount);
+        if (isInsertedAmountChanged && !isAmountClicked) {
+            const updatedMap = new Map(amountIndexClickedTimesMap);
+            // reset all amounts clicks
+            updatedMap.forEach((_, index, theMap) => theMap.set(index, 0));
+            setAmountIndexClickedTimesMap(updatedMap);
+        }
+        prevInsertedAmount.current = Number(insertedAmount);
+        setIsAmountClicked(false);
+    }, [insertedAmount, isAmountClicked, amountIndexClickedTimesMap]);
+
+    const onAmountClickHandler = (amountIndex: number, buyAmount: number) => {
+        setIsAmountClicked(true);
+        const updatedMap = new Map(amountIndexClickedTimesMap);
+        // reset clicks for all other amounts except for current index
+        updatedMap.forEach((clickedTimes, index, theMap) => {
+            index === amountIndex ? clickedTimes : theMap.set(index, 0);
+        });
+
+        const increasedAmountClicks = (updatedMap.get(amountIndex) || 0) + 1;
+        updatedMap.set(amountIndex, increasedAmountClicks);
+        setAmountIndexClickedTimesMap(updatedMap);
+
+        changeAmount(increasedAmountClicks * buyAmount);
+    };
 
     return (
         <Container>
@@ -45,15 +78,16 @@ const SuggestedAmount: React.FC<SuggestedAmountProps> = ({
                 const convertedAmount = convertFromStable(amount);
                 const buyAmount = minAmount && index === 0 && minAmount > convertedAmount ? minAmount : convertedAmount;
 
+                const isCurrentAmountClicked = (amountIndexClickedTimesMap.get(index) || 0) > 0;
+                const isAnyAmountClicked = amountIndexClickedTimesMap.values().some((n) => n > 0);
+
                 return (
                     <AmountContainer
                         key={`amount-${index}`}
-                        active={insertedAmount == buyAmount}
-                        onClick={() => {
-                            changeAmount(buyAmount);
-                        }}
+                        active={isCurrentAmountClicked || (!isAnyAmountClicked && Number(insertedAmount) === buyAmount)}
+                        onClick={() => onAmountClickHandler(index, buyAmount)}
                     >
-                        {formatCurrencyWithKey(USD_SIGN, amount, 1, true)}
+                        {`${isCurrentAmountClicked ? '+' : ''} ${formatCurrencyWithKey(USD_SIGN, amount, 1, true)}`}
                     </AmountContainer>
                 );
             })}
