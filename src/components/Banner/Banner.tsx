3cce7e23640bf01a6b25daf11706abbf04b76d7f
Sasa Lotina <lotinasa@gmail.com>
2025-02-25 16:05:36 +0100

    Hide speed banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,7 +5,7 @@ import { FlexDiv } from 'styles/common';
 import { buildSpeedMarketsBannerHref } from 'utils/routes';
 import SPAAnchor from '../SPAAnchor';
 
-const IS_VISIBLE = true;
+const IS_VISIBLE = false;
 
 const Banner: React.FC = () => {
     return !IS_VISIBLE ? (
