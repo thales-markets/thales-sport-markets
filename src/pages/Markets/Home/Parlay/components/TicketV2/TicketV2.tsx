433ef871c66b29a7e56491b389a29ba23f3d5f82
Sasa Lotina <lotinasa@gmail.com>
2025-03-04 13:34:16 +0100

    Fix default collateral currency rate

--- src/pages/Markets/Home/Parlay/components/TicketV2/TicketV2.tsx
+++ src/pages/Markets/Home/Parlay/components/TicketV2/TicketV2.tsx
@@ -467,7 +467,7 @@ const Ticket: React.FC<TicketProps> = ({
     }, [coingeckoRatesQuery, selectedCollateral, buyInAmount, networkId]);
 
     const selectedCollateralCurrencyRate =
-        exchangeRates && exchangeRates !== null ? exchangeRates[usedCollateralForBuy] : 1;
+        exchangeRates && exchangeRates !== null && !isDefaultCollateral ? exchangeRates[usedCollateralForBuy] : 1;
     const thalesContractCurrencyRate =
         exchangeRates && exchangeRates !== null ? exchangeRates[THALES_CONTRACT_RATE_KEY] : 1;
 
