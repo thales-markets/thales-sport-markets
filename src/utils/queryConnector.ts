40f6f6f3e8e4fc26bb148cb3d0ed80eed797b1e1
Sasa Lotina <lotinasa@gmail.com>
2025-04-04 12:02:58 +0200

    refetch notifications after buy

--- src/utils/queryConnector.ts
+++ src/utils/queryConnector.ts
@@ -54,6 +54,13 @@ export const refetchAfterClaim = (walletAddress: string, networkId: Network) => 
     queryConnector.queryClient.invalidateQueries({ queryKey: QUERY_KEYS.UserTickets(networkId, walletAddress) });
 };
 
+export const refetchAfterBuy = (walletAddress: string, networkId: Network) => {
+    queryConnector.queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.Wallet.MultipleCollateral(walletAddress, networkId),
+    });
+    queryConnector.queryClient.invalidateQueries({ queryKey: QUERY_KEYS.ClaimableCountV2(walletAddress, networkId) });
+};
+
 export const refetchLiquidityPoolData = (walletAddress: string, networkId: Network, liquidityPoolAddress: string) => {
     queryConnector.queryClient.invalidateQueries({
         queryKey: QUERY_KEYS.LiquidityPool.Data(liquidityPoolAddress, networkId),
