98b4b25d22841508738cd7e57e952e6841b44b0b
Vladan Thales <vladan@thalesmarket.io>
2025-05-13 09:44:01 +0200

    Add dots loader when proof not valid, check for market loading on sport filters

--- src/pages/Markets/Home/Parlay/components/TicketV2/TicketV2.tsx
+++ src/pages/Markets/Home/Parlay/components/TicketV2/TicketV2.tsx
@@ -2065,6 +2065,7 @@ const Ticket: React.FC<TicketProps> = ({
             return (
                 <Button disabled={true} onClick={() => {}} {...defaultButtonProps}>
                     {t('markets.parlay.latest-odds-checking')}
+                    <DotsLoader />
                 </Button>
             );
         }
@@ -2931,6 +2932,21 @@ const LogoIcon = styled.i`
     font-size: 14px;
     line-height: 12px;
     color: ${(props) => props.theme.status.win}!important;
+`;
+
+const DotsLoader = styled.div`
+    width: 20px;
+    aspect-ratio: 4;
+    margin-top: 6px;
+    margin-left: 2px;
+    background: radial-gradient(circle closest-side, #000 50%, #0000) 0 / calc(100% / 3) 100% space;
+    clip-path: inset(0 100% 0 0);
+    animation: l1 1s steps(4) infinite;
+    @keyframes l1 {
+        to {
+            clip-path: inset(0 -34% 0 0);
+        }
+    }
 `;
 
 export default Ticket;
