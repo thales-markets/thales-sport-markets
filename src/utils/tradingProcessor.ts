510db18b58444a18caf61a7ec139ecb21fe4b335
Gorshtak <sg31191@gmail.com>
2025-05-21 10:30:21 +0200

    removed console logs

--- src/utils/tradingProcessor.ts
+++ src/utils/tradingProcessor.ts
@@ -75,8 +75,6 @@ export const processTransaction = async (
     let counter = 0;
     const startTime = Date.now();
 
-    console.log('networkID: ', networkId);
-    console.log('tradingContract: ', tradingContract);
     let { isFulfilledTx, isFulfilledAdapter, isAdapterError } = await checkFulfilledTx(
         networkId,
         tradingContract,
