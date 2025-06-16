3aeac69288e40bd93161e2342160ef079eb310c8
Gorshtak <sg31191@gmail.com>
2025-06-16 13:48:37 +0200

    added farcaster walelt in mini app

--- src/constants/wallet.ts
+++ src/constants/wallet.ts
@@ -53,15 +53,19 @@ export const WALLETS_LABELS: { id: ParticalTypes | WalletConnections; labelKey: 
         id: WalletConnections.BINANCE,
         labelKey: 'common.wallet.particle-logins.binance-wallet',
     },
+    {
+        id: WalletConnections.FARCASTER,
+        labelKey: 'common.wallet.particle-logins.farcaster-wallet',
+    },
 ];
 
 export const BALANCE_THRESHOLD = 0.00000001;
-
+export const FARCASTER_ICON_URL =
+    'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBmaWxsPSIjODA1RUZGIiBkPSJNMTYgMEM3LjE2NCAwIDAgNy4xNjQgMCAxNnM3LjE2NCAxNiAxNiAxNnMxNi03LjE2NCAxNi0xNlMxOC44MzYgMCAxNiAwek0xMS40NjcgMjMuNDVMMTAuMDYgMjEuNThhLjYzNi42MzYgMCAwIDEgLjAyMy0uOTA4bDYuNDE4LTUuMzQ0YS40MjQuNDI0IDAgMCAwIDAtLjY0MkwxMC4wMyAxMC4zNDNhLjYzNi42MzYgMCAwIDEgLjA3LS45MDJsMS40MS0xLjg3MWEuNjM2LjYzNiAwIDAgMSAuODk1LS4xMThMMjIgMTUuNzA0YS44NDguODQ4IDAgMCAxIDAgMS4yOTJMMTIuNDE4IDIzLjU0OGEuNjM2LjYzNiAwIDAgMS0uOTUtLjA5OHoiLz48L3N2Zz4=';
 export const farcasterRainbowWallet = (options?: any): Wallet => ({
     id: 'farcaster',
     name: 'Farcaster Wallet',
-    iconUrl:
-        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBmaWxsPSIjODA1RUZGIiBkPSJNMTYgMEM3LjE2NCAwIDAgNy4xNjQgMCAxNnM3LjE2NCAxNiAxNiAxNnMxNi03LjE2NCAxNi0xNlMxOC44MzYgMCAxNiAwek0xMS40NjcgMjMuNDVMMTAuMDYgMjEuNThhLjYzNi42MzYgMCAwIDEgLjAyMy0uOTA4bDYuNDE4LTUuMzQ0YS40MjQuNDI0IDAgMCAwIDAtLjY0MkwxMC4wMyAxMC4zNDNhLjYzNi42MzYgMCAwIDEgLjA3LS45MDJsMS40MS0xLjg3MWEuNjM2LjYzNiAwIDAgMSAuODk1LS4xMThMMjIgMTUuNzA0YS44NDguODQ4IDAgMCAxIDAgMS4yOTJMMTIuNDE4IDIzLjU0OGEuNjM2LjYzNiAwIDAgMS0uOTUtLjA5OHoiLz48L3N2Zz4=', // Simple Farcaster-like SVG icon
+    iconUrl: FARCASTER_ICON_URL, // Base64 encoded SVG icon
     iconBackground: '#FFFFFF', // White background for the icon
     ...(options || {}),
     createConnector: () => farcasterFrame(),
