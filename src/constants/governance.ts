5842c0c3775255b48ea6b960fb058c1bf7b58269
Vladan Thales <vladan@thalesmarket.io>
2025-06-27 17:04:47 +0200

    Set elections modal

--- src/constants/governance.ts
+++ src/constants/governance.ts
@@ -1,4 +1,4 @@
 export const SNAPSHOT_GRAPHQL_URL = `https://hub.snapshot.org/graphql?apiKey=${process.env.VITE_APP_SNAPSHOT_API_KEY}`;
 export const SNAPSHOT_SCORE_URL = `https://score.snapshot.org/?apiKey=${process.env.VITE_APP_SNAPSHOT_API_KEY}`;
 
-export const VOTING_COUNCIL_PROPOSAL_ID = '0x124d74400a60cd5e9b74352af68df3ddf69eb3396b88527d64953dcee356f29a';
+export const VOTING_COUNCIL_PROPOSAL_ID = '0x3ab392fdc7d84df10cef6fd8e275263c88cdbdcdd15cec4352c92b2dc6518813';
