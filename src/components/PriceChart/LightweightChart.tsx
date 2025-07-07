f4293c3805368aa62f2d6271ffb31a97c908a996
Gorshtak <sg31191@gmail.com>
2025-07-07 10:46:41 +0200

    reduced size of pyth logo

--- src/components/PriceChart/LightweightChart.tsx
+++ src/components/PriceChart/LightweightChart.tsx
@@ -207,11 +207,11 @@ const Value = styled(Label)`
 const PythIconWrap = styled.div`
     position: absolute;
     right: 20px;
-    bottom: 0px;
+    bottom: 2px;
     z-index: 10;
     i {
-        font-size: 26px;
-        line-height: 26px;
+        font-size: 20px;
+        line-height: 20px;
         color: ${(props) => props.theme.textColor.primary};
     }
 `;
