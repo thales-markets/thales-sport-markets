cf0fe29c472d1dd74e7e0e916b1d7314af8a7f9a
Gorshtak <sg31191@gmail.com>
2025-08-05 16:22:41 +0200

    new affiliate  banner

--- src/queries/banners/useBannersQuery.ts
+++ src/queries/banners/useBannersQuery.ts
@@ -4,8 +4,6 @@ import esports from 'assets/images/banner-v2/esport.png';
 import farcaster from 'assets/images/banner-v2/farcaster.png';
 import overdropS2 from 'assets/images/banner-v2/overdrop-s2.png';
 import useOver from 'assets/images/banner-v2/use-over.png';
-import axios from 'axios';
-import { generalConfig } from 'config/general';
 import QUERY_KEYS from 'constants/queryKeys';
 import { NetworkConfig } from 'types/network';
 
@@ -23,11 +21,8 @@ export const useBannersQuery = (
         queryKey: QUERY_KEYS.Banners(networkConfig.networkId),
         queryFn: async () => {
             try {
-                const response = await axios.get(`${generalConfig.API_URL}/banners-v2/${networkConfig.networkId}`);
-                const mappedData = response.data.map((banner: Banner) => ({
-                    url: banner.url,
-                    image: `${generalConfig.API_URL}/banners-v2/image/${banner.image}`,
-                }));
+                // const response = await axios.get(`${generalConfig.API_URL}/banners-v2/${networkConfig.networkId}`);
+                const mappedData = [];
                 mappedData.push({
                     url:
                         'https://www.overtimemarkets.xyz/?sport=PlayerProps&status=OpenMarkets&lang=en-US&showActive=false&tag=CS2%2CDOTA+2%2CValorant%2CLOL',
