0c6d7249043083fa326f4f1a7001b82558b45a7d
vojin5rovic <vojinp13@gmail.com>
2025-08-06 12:57:29 +0200

    Fix daily quest finished checkmark on mobile

--- src/pages/Overdrop/components/DailyQuest/DailyQuest.tsx
+++ src/pages/Overdrop/components/DailyQuest/DailyQuest.tsx
@@ -345,8 +345,8 @@ const BadgeWrapper = styled(FlexDivCentered)`
 const FinishedContainer = styled(FlexDivCentered)`
     @media (max-width: ${ScreenSizeBreakpoint.SMALL}px) {
         position: absolute;
-        right: 5px;
-        bottom: 6px;
+        right: 2px;
+        bottom: 2px;
     }
 `;
 
