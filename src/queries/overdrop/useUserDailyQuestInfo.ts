f44de3f904f8197e84385de5f453c8d54db77fed
Gorshtak <sg31191@gmail.com>
2025-08-11 12:10:19 +0200

    add new query for overdrop daily quest

---
+++ src/queries/overdrop/useUserDailyQuestInfo.ts
@@ -0,0 +1,26 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import axios from 'axios';
+import { generalConfig } from 'config/general';
+import QUERY_KEYS from 'constants/queryKeys';
+import { OverdropUserData } from 'types/overdrop';
+
+const useUserDailyQuestInfo = (walletAddress: string, options?: Omit<UseQueryOptions<any>, 'queryKey' | 'queryFn'>) => {
+    return useQuery<OverdropUserData>({
+        queryKey: QUERY_KEYS.Overdrop.UserDailyQuestInfo(walletAddress),
+        queryFn: async () => {
+            try {
+                const response = await axios.get(
+                    `${generalConfig.OVERDROP_API_URL}/user-daily-quest-info/${walletAddress}`
+                );
+                if (response?.status === 200 && response?.data) return response.data;
+                return;
+            } catch (e) {
+                console.error(e);
+                return;
+            }
+        },
+        ...options,
+    });
+};
+
+export default useUserDailyQuestInfo;
