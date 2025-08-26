d8a630de2feeb9ad2207216d92d72c3103739f50
Gorshtak <sg31191@gmail.com>
2025-08-26 16:56:23 +0200

    add count per tag query

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -40,6 +40,7 @@ const QUERY_KEYS = {
         networkId,
         isLive,
     ],
+    SportMarketsCount: (networkId: Network) => ['sportMarketsCount', networkId],
     Prices: {
         PythPrices: (priceId: string, publishTime: number) => ['prices', 'pythPrices', priceId, publishTime],
         PythCandlestickData: (asset: string, dateRange: number, resolution: string) => [
---
+++ src/queries/markets/useCountPerTagQuery.ts
@@ -0,0 +1,28 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import axios from 'axios';
+import { generalConfig, noCacheConfig } from 'config/general';
+import QUERY_KEYS from 'constants/queryKeys';
+import { Network } from 'enums/network';
+
+const useCountPerTagQuery = (networkId: Network, options?: Omit<UseQueryOptions<any>, 'queryKey' | 'queryFn'>) => {
+    return useQuery<any | undefined>({
+        queryKey: QUERY_KEYS.SportMarketsCount(networkId),
+        queryFn: async () => {
+            try {
+                const response = await axios.get(
+                    `${generalConfig.API_URL}/overtime-v2/networks/${networkId}/markets/count-per-tag`,
+                    noCacheConfig
+                );
+                const data = response.data;
+
+                return data;
+            } catch (e) {
+                console.log(e);
+                return undefined;
+            }
+        },
+        ...options,
+    });
+};
+
+export default useCountPerTagQuery;
